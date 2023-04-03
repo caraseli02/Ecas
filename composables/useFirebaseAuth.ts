0dbcc4115121574c19b3f00ce15592f6ecaae615
Mohammad Sulthan <muhammadsultana@outlook.com>
2023-04-03 22:20:23 +0700

    feat: created get token function from firebase

--- composables/useFirebaseAuth.ts
+++ composables/useFirebaseAuth.ts
@@ -1,27 +1,49 @@
 import {
-    getRedirectResult,
+    getAdditionalUserInfo,
+    getIdToken,
     GoogleAuthProvider,
     signInWithPopup,
     User,
 } from "firebase/auth";
 
+interface FirebaseResults {
+    token: string | undefined;
+    user: User;
+    isNewUser: boolean | undefined
+}
+
+interface ParsedJWTToken {
+    aud: string
+    auth_time: number
+    email: string
+    email_verified: boolean
+    exp: number
+    iat: number
+    iss: string
+    name: string
+    picture: string
+    sub: string
+    user_id: string
+}
+
 export default function () {
     const { $auth } = useNuxtApp();
 
-    const user = useState<User | null>("google_user", () => null);
+    const user = useState<User | null>("google.com", () => null);
 
-    const registerUser = async (): Promise<unknown> => {
+    const registerUser = async (): Promise<FirebaseResults | { error: ErrorConstructor }> => {
         try {
             const provider = new GoogleAuthProvider();
-            const signIn = await signInWithPopup($auth, provider).then(
+            const signIn = await signInWithPopup($auth, provider)
+            .then(
                 (result) => {
-                    const credential =
-                        GoogleAuthProvider.credentialFromResult(result);
+                    const isNewUser = getAdditionalUserInfo(result)?.isNewUser
+                    const credential = GoogleAuthProvider.credentialFromResult(result);
                     const token = credential?.accessToken;
                     const user = result.user;
-                    return { token, user };
+                    return { token, user, isNewUser };
                 }
-            );
+            )
             return signIn;
         } catch (error: unknown) {
             if (error instanceof Error) {
@@ -31,8 +53,25 @@ export default function () {
         }
     };
 
+    const getUserJWTToken = async (): Promise<ParsedJWTToken> => {
+        const { currentUser } = $auth
+        const token = await getIdToken(currentUser)
+        return parseJwt(token)
+    }
+
+    function parseJwt (token: string): any {
+        let base64Url = token.split('.')[1];
+        let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        let JSONPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+
+        return JSON.parse(JSONPayload);
+    }
+
     return {
         user,
         registerUser,
+        getUserJWTToken
     };
 }
