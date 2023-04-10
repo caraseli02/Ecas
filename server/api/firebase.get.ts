3912e60bfbbffd0a1912560e687f236535462306
Mohammad Sulthan <muhammadsultana@outlook.com>
2023-04-11 01:51:08 +0700

    fix: removed export function

--- server/api/firebase.get.ts
+++ server/api/firebase.get.ts
@@ -1,4 +1,15 @@
-import firebaseConfig from "../utils/firebaseConfig";
 export default defineEventHandler(async (event) => {
-    return firebaseConfig()
+    const config = useRuntimeConfig();
+
+    const firebaseConfig = {
+        apiKey: config.FIREBASE_API_KEY,
+        authDomain: config.FIREBASE_AUTH_DOMAIN,
+        projectId: config.FIREBASE_PROJECT_ID,
+        storageBucket: config.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: config.FIREBASE_MESSAGE_SENDER_ID,
+        appId: config.FIREBASE_APP_ID,
+        measurementId: config.FIREBASE_MEASUREMENT_ID,
+    };
+
+    return firebaseConfig
 });
