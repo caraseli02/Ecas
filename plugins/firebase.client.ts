8527ad259e23646363f579f7fceed1034ec79370
Amzarescu Laurentiu <amzarescu41@gmail.com>
2023-04-11 00:56:08 +0300

    test

--- plugins/firebase.client.ts
+++ plugins/firebase.client.ts
@@ -3,6 +3,7 @@ import { getAuth } from "firebase/auth";
 
 export default defineNuxtPlugin(async (nuxtApp) => {
     const config = await $fetch("/api/firebase");
+    console.log(config);
 
     const app = initializeApp(config);
     const auth = getAuth(app);
--- server/api/firebase.get.ts
+++ server/api/firebase.get.ts
@@ -2,13 +2,13 @@ export default defineEventHandler(async (event) => {
     const config = useRuntimeConfig();
 
     const firebaseConfig = {
-        apiKey: config.FIREBASE_API_KEY || 'mock-key',
-        authDomain: config.FIREBASE_AUTH_DOMAIN,
-        projectId: config.FIREBASE_PROJECT_ID,
-        storageBucket: config.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: config.FIREBASE_MESSAGE_SENDER_ID,
-        appId: config.FIREBASE_APP_ID,
-        measurementId: config.FIREBASE_MEASUREMENT_ID,
+        apiKey: config.NUXT_FIREBASE_API_KEY,
+        authDomain: config.NUXT_FIREBASE_AUTH_DOMAIN,
+        projectId: config.NUXT_FIREBASE_PROJECT_ID,
+        storageBucket: config.NUXT_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: config.NUXT_FIREBASE_MESSAGE_SENDER_ID,
+        appId: config.NUXT_FIREBASE_APP_ID,
+        measurementId: config.NUXT_FIREBASE_MEASUREMENT_ID,
     };
 
     return firebaseConfig
