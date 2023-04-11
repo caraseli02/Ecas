a3ddc28cb02688c6302f2db80184d73170828f70
Amzarescu Laurentiu <amzarescu41@gmail.com>
2023-04-12 00:37:19 +0300

    test

--- plugins/firebase.client.ts
+++ plugins/firebase.client.ts
@@ -5,13 +5,13 @@ export default defineNuxtPlugin(async (nuxtApp) => {
     const config = useRuntimeConfig();
 
     const app = initializeApp({
-        apiKey: config.FIREBASE_API_KEY,
-        authDomain: config.FIREBASE_AUTH_DOMAIN,
-        projectId: config.FIREBASE_PROJECT_ID,
-        storageBucket: config.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: config.FIREBASE_MESSAGE_SENDER_ID,
-        appId: config.FIREBASE_APP_ID,
-        measurementId: config.FIREBASE_MEASUREMENT_ID,
+        apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+        authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+        projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+        storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGE_SENDER_ID,
+        appId: import.meta.env.VITE_FIREBASE_APP_ID,
+        measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
     });
 
 
