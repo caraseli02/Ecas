0de844744002e3054694721d20b82347598a9c68
Amzarescu Laurentiu <amzarescu41@gmail.com>
2023-04-11 20:51:07 +0300

    remove internal api endpoint

--- plugins/firebase.client.ts
+++ plugins/firebase.client.ts
@@ -2,10 +2,18 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 export default defineNuxtPlugin(async (nuxtApp) => {
-    const config = await $fetch("/api/firebase");
-    console.log(config);
+    const config = useRuntimeConfig();
 
-    const app = initializeApp(config);
+    const app = initializeApp({
+        apiKey: config.FIREBASE_API_KEY,
+        authDomain: config.FIREBASE_AUTH_DOMAIN,
+        projectId: config.FIREBASE_PROJECT_ID,
+        storageBucket: config.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: config.FIREBASE_MESSAGE_SENDER_ID,
+        appId: config.FIREBASE_APP_ID,
+        measurementId: config.FIREBASE_MEASUREMENT_ID,
+    });
+
     const auth = getAuth(app);
 
     nuxtApp.vueApp.provide("auth", auth);
