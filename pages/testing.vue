071511403cfc170e9c358a46b6003de33449329a
bokismoki <borisbosnjak084@gmail.com>
2023-05-08 20:38:24 +0200

    Truncate text size on ToastMessage

--- pages/testing.vue
+++ pages/testing.vue
@@ -2,19 +2,19 @@
   <div class="grid grid-cols-1 gap-10 container py-20">
     <ToastMessage
       type="info"
-      content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi magna augue, fringilla a ex non, suscipit egestas erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi magna augue, fringilla a ex non, suscipit egestas erat."
+      content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi magna augue, fringilla a ex non, suscipit egestas erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     />
     <ToastMessage
       type="warning"
-      content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi magna augue, fringilla a ex non, suscipit egestas erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi magna augue, fringilla a ex non, suscipit egestas erat."
+      content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi magna augue, fringilla a ex non, suscipit egestas erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     />
     <ToastMessage
       type="success"
-      content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi magna augue, fringilla a ex non, suscipit egestas erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi magna augue, fringilla a ex non, suscipit egestas erat."
+      content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi magna augue, fringilla a ex non, suscipit egestas erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     />
     <ToastMessage
       type="error"
-      content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi magna augue, fringilla a ex non, suscipit egestas erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi magna augue, fringilla a ex non, suscipit egestas erat."
+      content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi magna augue, fringilla a ex non, suscipit egestas erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     />
   </div>
 </template>
