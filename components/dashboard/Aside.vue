2de5452bda885cda3cc0809edde06fb7afbf0218
bokismoki <borisbosnjak084@gmail.com>
2023-07-10 21:07:33 +0200

    Reduce dashboard aside overlay height

--- components/dashboard/Aside.vue
+++ components/dashboard/Aside.vue
@@ -6,13 +6,13 @@
         <Transition name="fade">
             <div
                 v-if="showTopNavLayer && !isCollapsedOnDesktop"
-                class="absolute top-[106px] left-0 w-full h-[100px] pointer-events-none bg-gradient-to-t from-transparent to-[#1b1b28] md:top-[112px]"
+                class="absolute top-[106px] left-0 w-full h-[52px] pointer-events-none bg-gradient-to-t from-transparent to-[#1b1b28] md:top-[112px]"
             />
         </Transition>
         <Transition name="fade">
             <div
                 v-if="showBottomNavLayer && !isCollapsedOnDesktop"
-                class="absolute left-0 bottom-[153px] w-full h-[100px] pointer-events-none bg-gradient-to-b from-transparent to-[#1b1b28]"
+                class="absolute left-0 bottom-[153px] w-full h-[52px] pointer-events-none bg-gradient-to-b from-transparent to-[#1b1b28]"
             />
         </Transition>
         <div
