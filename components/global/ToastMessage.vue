b5c61d1b81404ceb3ba99503cc02a53453e45154
bokismoki <borisbosnjak084@gmail.com>
2023-08-19 17:42:09 +0200

    Update toast styles

--- components/global/ToastMessage.vue
+++ components/global/ToastMessage.vue
@@ -1,33 +1,42 @@
 <template>
     <div
-        class="rounded-lg overflow-hidden border-l-8 bg-white max-w-full w-[360px] shadow-xs md:w-[560px] lg:w-[680px]"
+        class="rounded-lg overflow-hidden border-l-[6px] bg-white max-w-full w-[360px] shadow-m py-2 md:w-[480px] md:h-14 lg:border-l-8 lg:w-[680px] lg:h-16"
         :class="[
             type === 'info' ? 'border-blue' : type === 'warning' ? 'border-yellow' : type === 'success' ? 'border-green' : 'border-red',
         ]"
     >
-        <div class="flex items-center space-x-4 px-4">
+        <div class="flex items-center space-x-3 pl-3 pr-4 md:space-x-4 lg:px-4">
             <div
-                class="flex items-center justify-center w-10 h-10 rounded-full text-white"
+                class="flex items-center justify-center w-10 h-10 rounded-full text-white lg:w-12 lg:h-12"
                 :class="[type === 'info' ? 'bg-blue' : type === 'warning' ? 'bg-yellow' : type === 'success' ? 'bg-green' : 'bg-red']"
             >
-                <InfoIcon v-if="type === 'info'" class="w-6 h-6" />
-                <WarningIcon v-else-if="type === 'warning'" class="w-6 h-6" />
-                <SuccessIcon v-else-if="type === 'success'" class="w-6 h-6" />
-                <ErrorIcon v-else class="w-6 h-6" />
+                <InfoIcon v-if="type === 'info'" class="w-6 h-6 lg:w-8 lg:h-8" />
+                <WarningIcon v-else-if="type === 'warning'" class="w-6 h-6 lg:w-8 lg:h-8" />
+                <SuccessIcon v-else-if="type === 'success'" class="w-6 h-6 lg:w-8 lg:h-8" />
+                <ErrorIcon v-else class="w-6 h-6 lg:w-8 lg:h-8" />
             </div>
-            <div class="flex-1 py-4">
-                <div class="capitalize leading-tight font-semibold mb-1">
+            <div class="flex-1">
+                <div class="capitalize leading-[1.42857] text-sm font-semibold lg:text-base lg:leading-[1.5] lg:mb-1">
                     {{ title || type }}
                 </div>
-                <div class="text-sm leading-[1.43]">
+                <div class="text-xs leading-[1.5] lg:text-sm lg:leading-[1.42857]">
                     {{ content }}
                 </div>
             </div>
             <button
-                class="rounded w-8 h-8 bg-[#F2F2F2] flex items-center justify-center text-gray-100 transition-colors duration-300 hover:text-gray-300"
+                class="rounded w-6 h-6 bg-[#F2F2F2] flex items-center justify-center text-gray-100 transition-colors duration-300 hover:text-gray-300 lg:w-8 lg:h-8"
                 @click="$emit('close')"
             >
-                <XIcon class="w-4 h-4" />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-4 h-4 lg:w-6 lg:h-6">
+                    <g id="vuesax/linear/add">
+                        <g id="add">
+                            <g id="Group 33824" stroke="#BDBDBD" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5">
+                                <path id="Vector" d="m5 5 14 14" />
+                                <path id="Vector_2" d="M5 19 19 5" />
+                            </g>
+                        </g>
+                    </g>
+                </svg>
             </button>
         </div>
     </div>
