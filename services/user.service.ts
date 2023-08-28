86595ce7a10fc867b1c6dbdf34b4be003130d6f2
tudorstanciulescu <32811555+tudorstanciulescu@users.noreply.github.com>
2023-08-29 00:18:00 +0300

    starting reset password

--- services/user.service.ts
+++ services/user.service.ts
@@ -1,6 +1,8 @@
 import HttpFactory from '@/composables/HttpFactory';
-import { PaginatedUserRequest } from '~/model/user/request/PaginatedUserRequest';
-import { useAuthStore } from '~/store/authStore';
+import {PaginatedUserRequest} from '~/model/user/request/PaginatedUserRequest';
+import {useAuthStore} from '~/store/authStore';
+import {ProductResponse} from '~/model/products/response/ProductResponse';
+import {FirebaseError} from 'firebase/app';
 
 class UserService extends HttpFactory {
     private RESOURCE = '/user';
@@ -9,7 +11,7 @@ class UserService extends HttpFactory {
 
     async fetchPaginatedUser(params: PaginatedUserRequest) {
         const baseURL = useRuntimeConfig().public.BASE_URL_API;
-        const { data, error } = await useFetchAPI(`${baseURL}/${this.RESOURCE}`, {
+        const {data, error} = await useFetchAPI(`${baseURL}/${this.RESOURCE}`, {
             headers: {
                 Authorization: `Bearer ${this.token}`,
             },
@@ -20,6 +22,17 @@ class UserService extends HttpFactory {
         }
 
         return data;
+    }
+
+    async resetPassword(email: string): Promise<ProductResponse | FirebaseError | unknown> {
+        try {
+            return await this.call('POST', `${this.RESOURCE}/password/email/reset`, {email: email});
+        } catch (err) {
+            if (err instanceof FirebaseError) {
+                return err;
+            }
+            return err;
+        }
     }
 }
 
