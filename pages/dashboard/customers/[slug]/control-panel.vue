c339cbea3a39c5b68f17e7406c8d123356f2f271
Amzarescu Laurentiu <amzarescu41@gmail.com>
2023-09-09 01:11:35 +0300

    add enum for control panel tabs; fix access for account and organization tabs

--- pages/dashboard/customers/[slug]/control-panel.vue
+++ pages/dashboard/customers/[slug]/control-panel.vue
@@ -1,160 +1,157 @@
 <template>
-  <div class="w-[1488px] max-w-full p-4 mx-auto transition-all duration-300 md:py-6 2xl:px-6">
-    <DashboardBreadcrumbs
-        title="Control Panel" :customer="customer" :panel-view="activeView"
-        :account-type="accountType">
-      <div class="max-lg:hidden max-w-max">
-        <div class="grid grid-cols-[repeat(2,auto)] gap-5 text-right">
-          <div class="flex flex-col">
-            <div class="text-sm leading-relaxed font-medium text-gray-300 mb-3">Credit Limit</div>
-            <div v-if="!isLoading" class="font-semibold leading-tight">
-              <div v-if="emptyData || error" class="text-sm font-medium leading-tight text-gray-100">No data available
-              </div>
-              <div v-else>€ 100,000.00</div>
+    <div class="w-[1488px] max-w-full p-4 mx-auto transition-all duration-300 md:py-6 2xl:px-6">
+        <DashboardBreadcrumbs title="Control Panel" :customer="customer" :panel-view="activeView" :account-type="accountType">
+            <div class="max-lg:hidden max-w-max">
+                <div class="grid grid-cols-[repeat(2,auto)] gap-5 text-right">
+                    <div class="flex flex-col">
+                        <div class="text-sm leading-relaxed font-medium text-gray-300 mb-3">Credit Limit</div>
+                        <div v-if="!isLoading" class="font-semibold leading-tight">
+                            <div v-if="emptyData || error" class="text-sm font-medium leading-tight text-gray-100">No data available</div>
+                            <div v-else>€ 100,000.00</div>
+                        </div>
+                        <SkeletonLoader v-else class="w-[104px] h-5" />
+                    </div>
+                    <div
+                        class="relative pl-5 before:absolute before:top-1/2 before:-translate-y-1/2 before:left-0 before:w-px before:h-[85%] before:bg-border before:rounded-[50px]"
+                    >
+                        <div class="text-sm leading-relaxed font-medium text-gray-300 mb-3">Available Credit</div>
+                        <div v-if="!isLoading" class="font-semibold leading-tight text-blue">
+                            <div v-if="emptyData || error" class="text-sm font-medium leading-tight text-gray-100">No data available</div>
+                            <div v-else>€ 45,328.63</div>
+                        </div>
+                        <SkeletonLoader v-else class="w-[104px] h-5" />
+                    </div>
+                </div>
             </div>
-            <SkeletonLoader v-else class="w-[104px] h-5"/>
-          </div>
-          <div
-              class="relative pl-5 before:absolute before:top-1/2 before:-translate-y-1/2 before:left-0 before:w-px before:h-[85%] before:bg-border before:rounded-[50px]"
-          >
-            <div class="text-sm leading-relaxed font-medium text-gray-300 mb-3">Available Credit</div>
-            <div v-if="!isLoading" class="font-semibold leading-tight text-blue">
-              <div v-if="emptyData || error" class="text-sm font-medium leading-tight text-gray-100">No data available
-              </div>
-              <div v-else>€ 45,328.63</div>
+        </DashboardBreadcrumbs>
+        <div class="grid grid-cols-1 gap-4 md:gap-6">
+            <div class="bg-white rounded-xl px-4 py-3 shadow-xs grid grid-cols-2 md:py-2 lg:hidden">
+                <div class="pr-4 md:flex md:items-center md:py-2">
+                    <SkeletonLoader v-if="isLoading" class="w-full h-[45px] md:h-5" />
+                    <template v-else>
+                        <div
+                            class="flex items-center justify-between text-xs leading-relaxed font-medium text-gray-300 mb-2 md:m-0 md:text-sm md:mr-2"
+                        >
+                            <div>Credit Limit</div>
+                            <WarningIcon v-if="error" class="w-5 h-5 md:hidden" />
+                        </div>
+                        <div v-if="emptyData || error" class="text-sm font-medium leading-tight text-gray-100">No data available</div>
+                        <div v-else class="text-sm font-semibold leading-tight">€ 100,000.00</div>
+                        <WarningIcon v-if="error" class="w-5 h-5 ml-auto max-md:hidden" />
+                    </template>
+                </div>
+                <div
+                    class="relative pl-4 before:absolute before:top-0 before:left-0 before:w-px before:h-full before:bg-gray-200 before:rounded-[50px] md:flex md:items-center"
+                >
+                    <SkeletonLoader v-if="isLoading" class="w-full h-[45px] md:h-5" />
+                    <template v-else>
+                        <div
+                            class="flex items-center justify-between text-xs leading-relaxed font-medium text-gray-300 mb-2 md:m-0 md:text-sm md:mr-2"
+                        >
+                            <div>Available Credit</div>
+                            <WarningIcon v-if="error" class="w-5 h-5 md:hidden" />
+                        </div>
+                        <div v-if="emptyData || error" v class="text-sm font-medium leading-tight text-gray-100">No data available</div>
+                        <div v-else class="text-sm font-semibold leading-tight text-blue">€ 45,328.63</div>
+                        <WarningIcon v-if="error" class="w-5 h-5 ml-auto max-md:hidden" />
+                    </template>
+                </div>
             </div>
-            <SkeletonLoader v-else class="w-[104px] h-5"/>
-          </div>
-        </div>
-      </div>
-    </DashboardBreadcrumbs>
-    <div class="grid grid-cols-1 gap-4 md:gap-6">
-      <div class="bg-white rounded-xl px-4 py-3 shadow-xs grid grid-cols-2 md:py-2 lg:hidden">
-        <div class="pr-4 md:flex md:items-center md:py-2">
-          <SkeletonLoader v-if="isLoading" class="w-full h-[45px] md:h-5"/>
-          <template v-else>
-            <div
-                class="flex items-center justify-between text-xs leading-relaxed font-medium text-gray-300 mb-2 md:m-0 md:text-sm md:mr-2"
-            >
-              <div>Credit Limit</div>
-              <WarningIcon v-if="error" class="w-5 h-5 md:hidden"/>
-            </div>
-            <div v-if="emptyData || error" class="text-sm font-medium leading-tight text-gray-100">No data available
-            </div>
-            <div v-else class="text-sm font-semibold leading-tight">€ 100,000.00</div>
-            <WarningIcon v-if="error" class="w-5 h-5 ml-auto max-md:hidden"/>
-          </template>
-        </div>
-        <div
-            class="relative pl-4 before:absolute before:top-0 before:left-0 before:w-px before:h-full before:bg-gray-200 before:rounded-[50px] md:flex md:items-center"
-        >
-          <SkeletonLoader v-if="isLoading" class="w-full h-[45px] md:h-5"/>
-          <template v-else>
-            <div
-                class="flex items-center justify-between text-xs leading-relaxed font-medium text-gray-300 mb-2 md:m-0 md:text-sm md:mr-2"
-            >
-              <div>Available Credit</div>
-              <WarningIcon v-if="error" class="w-5 h-5 md:hidden"/>
-            </div>
-            <div v-if="emptyData || error" v class="text-sm font-medium leading-tight text-gray-100">No data available
-            </div>
-            <div v-else class="text-sm font-semibold leading-tight text-blue">€ 45,328.63</div>
-            <WarningIcon v-if="error" class="w-5 h-5 ml-auto max-md:hidden"/>
-          </template>
-        </div>
-      </div>
-      <div class="relative flex items-center gap-1 bg-white rounded-xl p-2 shadow-xs overflow-x-auto hide-scrollbar">
-        <NuxtLink
-            v-for="(view, index) in panelViews"
-            :key="index"
-            :to="`/dashboard/customers/${route.params.slug}/control-panel/${view
-                        .toLowerCase()
-                        .replace(/ /g, '-')}`"
-            class="panelView relative z-10 text-sm font-medium leading-[1.42857] px-6 py-3 rounded-lg flex-shrink-0 transition-colors duration-300"
-            :class="[view.toLowerCase().replace(/ /, '-') === activeView ? 'text-white ' : 'hover:text-blue']"
-        >
-          {{ view }}
-        </NuxtLink>
-        <div
-            class="absolute top-1/2 -translate-y-1/2 bg-blue h-11 rounded-lg transition-all duration-300"
-            :style="{
+            <div class="relative flex items-center gap-1 bg-white rounded-xl p-2 shadow-xs overflow-x-auto hide-scrollbar">
+                <NuxtLink
+                    v-for="(view, index) in panelViews.filter((item) => item.allowTo.includes(accountType))"
+                    :key="index"
+                    :to="`/dashboard/customers/${route.params.slug}/control-panel/${view.path}`"
+                    class="panelView relative z-10 text-sm font-medium leading-[1.42857] px-6 py-3 rounded-lg flex-shrink-0 transition-colors duration-300"
+                    :class="[view.path === activeView ? 'text-white ' : 'hover:text-blue']"
+                >
+                    {{ view.name }}
+                </NuxtLink>
+                <div
+                    class="absolute top-1/2 -translate-y-1/2 bg-blue h-11 rounded-lg transition-all duration-300"
+                    :style="{
                         width: viewHighlightWidth + 'px',
                         left: viewHightlightLeft + 'px',
                     }"
-        />
-      </div>
-      <NuxtPage/>
+                />
+            </div>
+            <NuxtPage />
+        </div>
     </div>
-  </div>
 </template>
 
 <script setup lang="ts">
-import {UserDetails} from '~/types/auth/user-details';
-import {useNuxtApp} from '#app';
+import { UserDetails } from '~/types/auth/user-details';
+import { useNuxtApp } from '#app';
+import { ControlPanelLabels, ControlPanelTabsEnum } from '~/types/dashboard/control-panel';
+import { AccountType } from '~/types';
 
 const route = useRoute();
 const router = useRouter();
 
 useHead({
-  title: 'Dashboard Control Panel',
+    title: 'Dashboard Control Panel',
 });
 
 const customer = ref('');
-
-
 const viewHighlightWidth = ref(0);
 const viewHightlightLeft = ref(0);
-const {$api} = useNuxtApp();
+const { $api } = useNuxtApp();
 const activeView = computed(() => {
-  return route.params.view;
+    return route.params.view;
 });
-const accountType = ref(0)
-const setActivePanelHighlight = () => {
-  const view = activeView.value;
-  const index = panelViews.findIndex((panelView) => panelView.toLowerCase().replace(/ /g, '-') === view);
-  if (index !== -1) {
-    const viewElement = document.querySelectorAll('.panelView')[index] as HTMLElement;
+const accountType = ref(0);
+const panelViews = ControlPanelLabels;
 
-    if (viewElement) {
-      viewHighlightWidth.value = viewElement.clientWidth;
-      viewHightlightLeft.value = viewElement.offsetLeft;
+const setActivePanelHighlight = () => {
+    const view = activeView.value;
+    const index = panelViews.findIndex((panelView) => panelView.path === view) - 1;
+
+    if (index !== -1) {
+        const viewElement = document.querySelectorAll('.panelView')[index] as HTMLElement;
+
+        if (viewElement) {
+            viewHighlightWidth.value = viewElement.clientWidth;
+            viewHightlightLeft.value = viewElement.offsetLeft;
+        }
     }
-  }
 };
 
 watch(activeView, () => {
-  setActivePanelHighlight();
+    setActivePanelHighlight();
 });
 
 const fetchInformation = async () => {
+    if (!route.params.slug) {
+        return;
+    }
 
-  const response = (await $api.customerProfile.fetchCustomerInformation(route.params.slug || '')) as {
-    status: string;
-    data: UserDetails;
-  };
+    const response = (await $api.customerProfile.fetchCustomerInformation(route.params.slug as string)) as {
+        status: string;
+        data: UserDetails;
+    };
 
-  if (response.status !== 'success') {
-    return;
-  }
-  accountType.value = response.data.accountType as any;
+    if (response.status !== 'success') {
+        return;
+    }
+    accountType.value = response.data.accountType as AccountType;
 };
 
-await fetchInformation()
-const panelViews = [(accountType.value == 0 ? 'Account' : 'Organization'), 'Shipping', 'Billing', 'Transaction History', 'Settings'];
-console.log(panelViews)
-onMounted(() => {
-  if (!route.params.view) {
-    const fixedPath = route.path.replace(/\/+$/, '');
-    const newPath = `${fixedPath}/${accountType.value == 0 ? 'Account' : 'Organization'}`;
+await fetchInformation();
 
-    router.replace({
-      path: newPath,
-    });
-  }
-  setActivePanelHighlight();
+onMounted(() => {
+    if (!route.params.view) {
+        const fixedPath = route.path.replace(/\/+$/, '');
+        const newPath = `${fixedPath}/${accountType.value == 0 ? ControlPanelTabsEnum.Account : ControlPanelTabsEnum.Organization}`;
+
+        router.replace({
+            path: newPath,
+        });
+    }
+    setActivePanelHighlight();
 });
 
 definePageMeta({
-  layout: 'dashboard',
+    layout: 'dashboard',
 });
 </script>
--- pages/dashboard/customers/[slug]/control-panel/[view].vue
+++ pages/dashboard/customers/[slug]/control-panel/[view].vue
@@ -1,35 +1,75 @@
 <template>
-  <div>
-    <div v-if="activeView !== 'settings'" class="bg-white rounded-xl px-4 py-6 shadow-xs md:px-6">
-
-      <DashboardControlPanelAccountView v-if="(activeView === 'account')"/>
-      <DashboardControlPanelOrganizationView v-if="(activeView === 'organization')"/>
-      <DashboardControlPanelShippingView v-else-if="activeView === 'shipping'"/>
-      <DashboardControlPanelTransactionHistoryView v-else-if="activeView === 'transaction-history'"/>
-      <DashboardControlPanelBillingView v-else-if="activeView === 'billing'"/>
+    <div>
+        <div v-if="activeView !== ControlPanelTabsEnum.Settings" class="bg-white rounded-xl px-4 py-6 shadow-xs md:px-6">
+            <DashboardControlPanelOrganizationView
+                v-if="activeView === ControlPanelTabsEnum.Organization && customerDetails.accountType !== AccountType.Personal"
+            />
+            <DashboardControlPanelAccountView
+                v-else-if="activeView === ControlPanelTabsEnum.Account && customerDetails.accountType === AccountType.Personal"
+            />
+            <DashboardControlPanelShippingView v-else-if="activeView === ControlPanelTabsEnum.Shipping" />
+            <DashboardControlPanelTransactionHistoryView v-else-if="activeView === ControlPanelTabsEnum.TransactionHistory" />
+            <DashboardControlPanelBillingView v-else-if="activeView === ControlPanelTabsEnum.Billing" />
+        </div>
+        <DashboardControlPanelSettingsView v-if="activeView === ControlPanelTabsEnum.Settings" />
     </div>
-    <DashboardControlPanelSettingsView v-if="activeView === 'settings'"/>
-  </div>
 </template>
 
 <script setup lang="ts">
+import { ControlPanelTabs, ControlPanelTabsEnum } from '~/types/dashboard/control-panel';
+import { UserDetails } from '~/types/auth/user-details';
+import { useNuxtApp } from '#app';
+import { AccountType } from '~/types';
+
+const { $api } = useNuxtApp();
+
 const route = useRoute();
 const router = useRouter();
 
 const activeView = computed(() => {
-
-  return route.params.view as 'account' | 'organization' | 'shipping' | 'billing' | 'transaction-history' | 'settings';
+    return route.params.view as ControlPanelTabs;
 });
+
+const customerDetails = ref({} as UserDetails);
 
 onMounted(() => {
-  if (!['account', 'organization', 'shipping', 'billing', 'transaction-history', 'settings'].includes(route.params.view as string)) {
-    router.replace({
-      path: `/dashboard/customers/${route.params.slug}/control-panel/settings`,
-    });
-  }
+    if (!Object.values(ControlPanelTabsEnum).includes(route.params.view as ControlPanelTabsEnum)) {
+        router.replace({
+            path: `/dashboard/customers/${route.params.slug}/control-panel/${ControlPanelTabsEnum.Organization}`,
+        });
+    } else {
+        if (customerDetails.value.accountType === AccountType.Personal && route.params.view === ControlPanelTabsEnum.Organization) {
+            router.replace({
+                path: `/dashboard/customers/${route.params.slug}/control-panel/${ControlPanelTabsEnum.Account}`,
+            });
+        } else if (customerDetails.value.accountType !== AccountType.Personal && route.params.view === ControlPanelTabsEnum.Account) {
+            router.replace({
+                path: `/dashboard/customers/${route.params.slug}/control-panel/${ControlPanelTabsEnum.Organization}`,
+            });
+        }
+    }
 });
 
+const fetchInformation = async () => {
+    if (!route.params.slug) {
+        return;
+    }
+
+    const response = (await $api.customerProfile.fetchCustomerInformation(route.params.slug as string)) as {
+        status: string;
+        data: UserDetails;
+    };
+
+    if (response.status !== 'success') {
+        return;
+    }
+
+    customerDetails.value = response.data;
+};
+
+await fetchInformation();
+
 definePageMeta({
-  layout: 'dashboard',
+    layout: 'dashboard',
 });
 </script>
