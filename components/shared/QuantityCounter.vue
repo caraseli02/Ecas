dbb1ce0d253fa85959d8eae50642ecc19784b675
Robert <robertandrei.oprescu@gmail.com>
2023-12-02 22:13:35 +0200

    quantity counter fixed

--- components/shared/QuantityCounter.vue
+++ components/shared/QuantityCounter.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="flex flex-row">
+    <div class="flex flex-row bg-[#EBEBEB] rounded-lg">
         <button @click="decreaseValue()" class="p-1 bg-[#EBEBEB] rounded-l-lg group">
             <MinusGrey class="text-[#5E6278] group-hover:text-[#007FFF] transition duration-150" />
         </button>
-        <div class="min-w-[48px] h-[36px] items-center flex justify-center bg-[#F5F5F5F2]">
-            <input type="number" v-model="inputValue" @change="updateValue" class="text-center px-1 bg-transparent outline-none w-[48px]"/>
+        <div class="min-w-[56px] h-[36px] items-center flex justify-center bg-[#F5F5F5F2]">
+            <input type="number" v-model="inputValue" @change="updateValue" class="text-center text-sm font-normal leading-9 bg-transparent outline-none w-[48px]"/>
         </div>
         <button @click="increaseValue()" class="p-1 bg-[#EBEBEB] rounded-r-lg group">
             <PlusGrey class="text-[#5E6278] group-hover:text-[#007FFF] transition duration-150" />
