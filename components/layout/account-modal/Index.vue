3d0235a3f36b0a383b2a40f8eb6e9be42a2ca0c4
Vlad <caraseli02@gmail.com>
2023-12-14 21:26:15 +0100

    fix: menu signOut and formating

--- components/layout/account-modal/Index.vue
+++ components/layout/account-modal/Index.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="fixed z-[60] top-0 left-0 w-screen max-h-screen overflow-hidden pointer-events-none">
-        <div class="relative z-10 flex flex-col ml-auto w-full bg-white h-full pointer-events-auto md:w-[450px] p-4 md:p-6 scrollbar-thin">
+        <div
+            class="relative z-10 flex flex-col ml-auto w-full bg-white h-full pointer-events-auto md:w-[450px] p-4 md:p-6 scrollbar-thin">
             <div class="flex flex-col items-center justify-start pb-6 gap-6">
                 <button
                     class="rounded w-8 h-8 bg-[#F2F2F2] flex items-center justify-center text-gray-100 transition-colors duration-300 hover:text-gray-300 self-end"
-                    @click="$emit('close')"
-                >
+                    @click="$emit('close')">
                     <XIcon class="w-[24px] h-[24px] text-[#5E6278]" />
                 </button>
                 <LayoutAccountModalDetails v-if="getUserDetails" />
@@ -26,28 +26,25 @@
                 >
                     <span class="text-sm font-medium">Sign out</span>
                 </button> -->
-                
-                
+
+
             </div>
             <div class="text-xs text-gray-300 text-center mt-auto pb-2.5 pt-6 flex flex-col gap-6">
                 <button
-                    class="sticky bottom-0 w-full justify-center items-center self-stretch flex flex-col px-16 py-2 rounded-lg border-[1.5px] border-solid border-sky-500"
-                >
-                    <div class="flex items-center gap-2">
-                    <SignOutIcon class="w-6 h-6" />
-                    <div
-                        class="text-sky-500 text-base font-medium leading-7 self-stretch grow whitespace-nowrap"
-                    >
-                        Sign Out
-                    </div>
-                    </div>
+                    class="sticky bottom-0 w-full justify-center items-center self-stretch flex flex-col px-16 py-2 rounded-lg border-[1.5px] border-solid border-sky-500">
+                    <button class="flex items-center gap-2" @click="handleSignOut">
+                        <SignOutIcon class="w-6 h-6" />
+                        <div class="text-sky-500 text-base font-medium leading-7 self-stretch grow whitespace-nowrap">
+                            Sign Out
+                        </div>
+                    </button>
                 </button>
                 <p class="flex justify-center items-center gap-2">
                     <SvgoMenuHelp />
                     For assistance please contact
-                <a href="mailto:support@ecas.ro" class="text-blue hover:underline"> support@ecas.ro </a>
+                    <a href="mailto:support@ecas.ro" class="text-blue hover:underline"> support@ecas.ro </a>
                 </p>
-                </div>
+            </div>
         </div>
     </div>
 </template>
