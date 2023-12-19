925f3733cf399a4a261583fa6a35d525bd36acd1
tudorstanciulescu <32811555+tudorstanciulescu@users.noreply.github.com>
2023-12-19 02:33:20 +0200

    delete and warning fixed on order summary

--- components/order-summary/TableItem.vue
+++ components/order-summary/TableItem.vue
@@ -1,96 +1,106 @@
 <template>
-    <div class="rounded-lg" :class="expanded ? 'custom-shadow' : ''">
-        <div
-            class="relative cursor-pointer xl:cursor-default flex flex-row justify-between p-4 bg-[#EBEBEB] w-full border-[1px] border-[#EBEBEB] header-transition"
-            :class="expanded ? 'rounded-t-lg' : 'rounded-lg'"
-            @click="handleExpand"
-        >
-            <div class="flex flex-row truncate">
-                <label class="flex cursor-pointer mr-4" @click.stop>
-                    <input :value="item.selected" type="checkbox" class="sr-only" @change="handleCheck" />
-                    <div
-                        class="flex items-center justify-center flex-shrink-0 w-[18px] h-[18px] rounded mt-px border transition-colors duration-300"
-                        :class="[
+  <div class="rounded-lg" :class="expanded ? 'custom-shadow' : ''">
+    <div
+        class="relative cursor-pointer xl:cursor-default flex flex-row justify-between p-4 bg-[#EBEBEB] w-full border-[1px] border-[#EBEBEB] header-transition"
+        :class="expanded ? 'rounded-t-lg' : 'rounded-lg'"
+        @click="handleExpand"
+    >
+      <div class="flex flex-row truncate">
+        <label class="flex cursor-pointer mr-4" @click.stop>
+          <input :value="item.selected" type="checkbox" class="sr-only" @change="handleCheck"/>
+          <div
+              class="flex items-center justify-center flex-shrink-0 w-[18px] h-[18px] rounded mt-px border transition-colors duration-300"
+              :class="[
                             item.selected
                                 ? 'bg-blue border-blue group-hover:bg-white'
                                 : 'bg-white  border-border group-hover:border-gray-300',
                         ]"
-                    >
-                        <CheckIcon v-if="item.selected" class="w-4 text-white transition-colors duration-300 group-hover:text-blue" />
-                    </div>
-                </label>
-                <div class="max-w-[220px] lg:max-w-[245px] flex">
+          >
+            <CheckIcon
+                v-if="item.selected"
+                class="w-4 text-white transition-colors duration-300 group-hover:text-blue"/>
+          </div>
+        </label>
+        <div class="max-w-[220px] lg:max-w-[245px] flex">
                     <span
                         v-if="stockItem"
                         class="truncate text-[#222] text-sm leading-6 font-normal self-center justify-self-center"
                         :class="expanded && shortStock ? 'hidden sm:block' : 'block'"
-                        >{{ item.productEntity?.alias }}</span
+                    >{{ item.productEntity?.alias }}</span
                     >
-                    <span v-else class="block truncate text-[#FA4B4B] text-sm font-normal leading-6 self-center justify-self-center">{{
-                        item.id
-                    }}</span>
-                </div>
-                <div v-if="shortStock" class="flex flex-row">
-                    <OrangeCheckCircle class="w-5 ml-2 sm:ml-4" :class="expanded ? 'ml-0' : ''" />
-                    <span
-                        class="text-[#FF8A00] text-sm font-medium leading-6 self-center pl-2"
-                        :class="expanded ? 'flex sm:hidden' : 'hidden'"
-                        >{{ item.productEntity.stock }} in stock</span
-                    >
-                </div>
-                <GreenCheckCircle v-if="!shortStock && stockItem" class="w-5 ml-2 sm:ml-4" />
-                <div v-if="!stockItem" class="flex flex-row items-center ml-4">
-                    <WarningErrorHuge class="hidden lg:block" />
-                    <WarningError class="block lg:hidden" />
-                    <span class="hidden sm:flex text-sm font-medium leading-6 text-[#FA4B4B] ml-2"
-                        >{{ item.stock - item.productEntity?.stock }} items will be on back order.</span
-                    >
-                </div>
-                <div
-                    v-if="stockItem && item.discount.value && discounts?.productDiscount"
-                    class="px-2 border-[1px] rounded-[25px] border-[#FA4B4B] bg-white ml-4 sm:flex items-center hidden"
-                >
-                    <span class="text-xs font-semibold leading-5 text-[#FA4B4B]">{{ item.discount.value }} %</span>
-                </div>
-            </div>
-            <div>
-                <div class="hidden xl:flex flex-row gap-4 ml-4">
-                    <Tooltip theme="black" position="top">
-                        <button class="relative flex group" @click="addToFavs">
-                            <HeartSolid v-show="item.liked" class="absolute r-0 top-[1px] text-[#007FFF]" />
-                            <HeartOutline class="w-5 h-5 text-[#5E6278] group-hover:text-[#007FFF] transition duration-150" />
-                        </button>
-                        <template #content>
-                            <span class="capitalize">Add to Favorites</span>
-                        </template>
-                    </Tooltip>
-                    <Tooltip theme="black" position="top">
-                        <button class="flex group" @click="deleteCurrent">
-                            <TrashOutline class="text-[#5E6278] group-hover:text-[#FA4B4B] transition duration-150" />
-                        </button>
-                        <template #content>
-                            <span class="capitalize">Delete</span>
-                        </template>
-                    </Tooltip>
-                </div>
-                <ChevronDownIcon
-                    class="w-5 h-5 flex-shrink-0 roun ded-full transition duration-300 xl:hidden flex"
-                    :class="[expanded ? 'rotate-180 text-[#007FFF]' : 'text-gray-300']"
-                />
-            </div>
+          <span
+              v-else
+              class="block truncate text-[#FA4B4B] text-sm font-normal leading-6 self-center justify-self-center">{{
+              item.id
+            }}</span>
         </div>
-        <Transition name="expand">
-            <OrderSummaryTableItemDropdown
-                v-if="expanded"
-                ref="tab"
-                :item="item"
-                :short-stock="shortStock"
-                :stock-item="stockItem"
-                :liked="liked"
-                class="item"
-            />
-        </Transition>
+        <div v-if="shortStock" class="flex flex-row">
+          <OrangeCheckCircle class="w-5 ml-2 sm:ml-4" :class="expanded ? 'ml-0' : ''"/>
+          <span
+              class="text-[#FF8A00] text-sm font-medium leading-6 self-center pl-2"
+              :class="expanded ? 'flex sm:hidden' : 'hidden'"
+          >{{ item.productEntity.stock }} in stock</span
+          >
+        </div>
+        <GreenCheckCircle v-if="!shortStock && stockItem" class="w-5 ml-2 sm:ml-4"/>
+        <div v-if="!stockItem" class="flex flex-row items-center ml-4">
+          <WarningErrorHuge class="hidden lg:block"/>
+          <WarningError class="block lg:hidden"/>
+          <span class="hidden sm:flex text-sm font-medium leading-6 text-[#FA4B4B] ml-2"
+          >{{ item.stock - item.productEntity?.stock }} items will be on back order.</span
+          >
+        </div>
+        <div
+            v-if="stockItem && item.discount.value && discounts?.productDiscount"
+            class="px-2 border-[1px] rounded-[25px] border-[#FA4B4B] bg-white ml-4 sm:flex items-center hidden"
+        >
+          <span class="text-xs font-semibold leading-5 text-[#FA4B4B]">{{ item.discount.value }} %</span>
+        </div>
+      </div>
+      <div>
+        <div class="hidden xl:flex flex-row gap-4 ml-4">
+          <Tooltip theme="black" position="top">
+            <button class="relative flex group" @click="addToFavs">
+              <HeartSolid v-show="item.liked" class="absolute r-0 top-[1px] text-[#007FFF]"/>
+              <HeartOutline class="w-5 h-5 text-[#5E6278] group-hover:text-[#007FFF] transition duration-150"/>
+            </button>
+            <template #content>
+              <span class="capitalize">Add to Favorites</span>
+            </template>
+          </Tooltip>
+          <button
+              v-if="!inModal"
+              class="flex items-center justify-center ml-auto text-gray-300 transition-colors duration-300 hover:text-[#FA4B4B]"
+              @click="deleteItem = true"
+          >
+            <TrashIcon class="w-5 h-5"/>
+          </button>
+        </div>
+        <ChevronDownIcon
+            class="w-5 h-5 flex-shrink-0 roun ded-full transition duration-300 xl:hidden flex"
+            :class="[expanded ? 'rotate-180 text-[#007FFF]' : 'text-gray-300']"
+        />
+      </div>
     </div>
+    <Transition name="expand">
+      <OrderSummaryTableItemDropdown
+          v-if="expanded"
+          ref="tab"
+          :item="item"
+          :short-stock="shortStock"
+          :stock-item="stockItem"
+          :liked="liked"
+          class="item"
+      />
+    </Transition>
+  </div>
+  <Teleport to="body">
+    <Transition name="slide-from-top">
+      <LayoutFavoritesModalsDelete
+          v-if="deleteItem" :products="[item]" :delete-from-cart="true"
+          @close="deleteItem = false"/>
+    </Transition>
+  </Teleport>
 </template>
 <script lang="ts">
 import CheckIcon from '@/assets/icons/check.svg';
@@ -103,129 +113,132 @@ import HeartOutline from '@/assets/icons/heart-outline.svg';
 import HeartSolid from '@/assets/icons/heart-solid.svg';
 import TrashOutline from '@/assets/icons/trash-outline.svg';
 import Tooltip from '~/components/global/Tooltip.vue';
-import { useAuthStore } from '~/store/authStore';
-import { storeToRefs } from 'pinia';
-import { parseProductPriceConfiguration } from '~/helpers/prices.helper';
+import {useAuthStore} from '~/store/authStore';
+import {storeToRefs} from 'pinia';
+import {parseProductPriceConfiguration} from '~/helpers/prices.helper';
+import TrashIcon from 'assets/icons/trash-can.svg';
 
 export default defineComponent({
-    name: 'TableItem',
-    components: {
-        CheckIcon,
-        OrangeCheckCircle,
-        GreenCheckCircle,
-        ChevronDownIcon,
-        WarningError,
-        WarningErrorHuge,
-        HeartOutline,
-        HeartSolid,
-        TrashOutline,
-        Tooltip,
+  name: 'TableItem',
+  components: {
+    TrashIcon,
+    CheckIcon,
+    OrangeCheckCircle,
+    GreenCheckCircle,
+    ChevronDownIcon,
+    WarningError,
+    WarningErrorHuge,
+    HeartOutline,
+    HeartSolid,
+    TrashOutline,
+    Tooltip,
+  },
+  props: {
+    item: {
+      type: Object,
+      required: true,
     },
-    props: {
-        item: {
-            type: Object,
-            required: true,
-        },
-        stockItem: {
-            type: Boolean as PropType<boolean>,
-            required: true,
-        },
+    stockItem: {
+      type: Boolean as PropType<boolean>,
+      required: true,
     },
-    data() {
-        return {
-            checked: false,
-            expanded: false,
-            thresholdStock: 10,
-            liked: false,
-        };
+  },
+  data() {
+    return {
+      checked: false,
+      expanded: false,
+      thresholdStock: 10,
+      liked: false,
+      deleteItem: false
+    };
+  },
+  computed: {
+    shortStock() {
+      return Number(this.item.productEntity?.stock) <= this.thresholdStock && this.item.productEntity?.stock > 0 && this.stockItem;
     },
-    computed: {
-        shortStock() {
-            return Number(this.item.productEntity?.stock) <= this.thresholdStock && this.item.productEntity?.stock > 0 && this.stockItem;
-        },
-        itemQuantity() {
-            return Number(this.item.stock);
-        },
-        discounts() {
-            if (!this.item.productEntity) {
-                return;
-            }
+    itemQuantity() {
+      return Number(this.item.stock);
+    },
+    discounts() {
+      if (!this.item.productEntity) {
+        return;
+      }
 
-            const authStore = useAuthStore();
-            const { getUserDetails } = storeToRefs(authStore);
+      const authStore = useAuthStore();
+      const {getUserDetails} = storeToRefs(authStore);
 
-            const discountsHelper = parseProductPriceConfiguration(this.item.productEntity, getUserDetails.value);
+      const discountsHelper = parseProductPriceConfiguration(this.item.productEntity, getUserDetails.value);
 
-            return {
-                userDiscount: discountsHelper.userDiscount,
-                productDiscount: discountsHelper.productDiscount,
-            };
-        },
+      return {
+        userDiscount: discountsHelper.userDiscount,
+        productDiscount: discountsHelper.productDiscount,
+      };
     },
-    watch: {
-        itemQuantity() {
-            if (this.item.stock) {
-                this.$emit('updateQuantity');
-            }
-        },
+  },
+  watch: {
+    itemQuantity() {
+      if (this.item.stock) {
+        this.$emit('updateQuantity');
+      }
     },
-    mounted() {
-        const mediaQuery = window.matchMedia('(min-width: 1280px)');
-        const handler = (e: MediaQueryListEvent) => {
-            this.expanded = e.matches;
-        };
-        mediaQuery.addEventListener('change', handler);
-        handler({ matches: mediaQuery.matches } as MediaQueryListEvent);
+  },
+  mounted() {
+    const mediaQuery = window.matchMedia('(min-width: 1280px)');
+    const handler = (e: MediaQueryListEvent) => {
+      this.expanded = e.matches;
+    };
+    mediaQuery.addEventListener('change', handler);
+    handler({matches: mediaQuery.matches} as MediaQueryListEvent);
+  },
+  methods: {
+    handleCheck() {
+      this.item.selected = !this.item.selected;
+      this.$emit('check', this.item);
     },
-    methods: {
-        handleCheck() {
-            this.item.selected = !this.item.selected;
-            this.$emit('check', this.item);
-        },
-        handleExpand() {
-            if (window.matchMedia('(max-width: 1280px)').matches) {
-                this.expanded = !this.expanded;
-            }
-        },
-        addToFavs() {
-            this.item.liked = !this.item.liked;
-        },
-        deleteCurrent() {
-            this.$emit('deleteCurrent', this.item.id);
-        },
+    handleExpand() {
+      if (window.matchMedia('(max-width: 1280px)').matches) {
+        this.expanded = !this.expanded;
+      }
     },
+    addToFavs() {
+      this.item.liked = !this.item.liked;
+    },
+    deleteCurrent() {
+      this.$emit('deleteCurrent', this.item.id);
+    },
+  },
 });
 </script>
 <style scoped>
 .expand-enter-from,
 .expand-leave-to {
-    opacity: 0.5;
-    max-height: 0;
-    overflow: hidden;
+  opacity: 0.5;
+  max-height: 0;
+  overflow: hidden;
 }
 
 .expand-enter-to,
 .expand-leave-from {
-    opacity: 1;
-    max-height: 1000px; /* Adjust this value as needed */
+  opacity: 1;
+  max-height: 1000px; /* Adjust this value as needed */
 }
 
 .expand-enter-active,
 .expand-leave-active {
-    transition: max-height 0.5s cubic-bezier(0, 0.3, 0.6, 1), opacity 0.5s ease;
+  transition: max-height 0.5s cubic-bezier(0, 0.3, 0.6, 1), opacity 0.5s ease;
 }
 
 .item {
-    transform-origin: top;
+  transform-origin: top;
 }
 
 .header-transition {
-    transition-property: box-shadow, border-radius, opacity;
-    transition-timing-function: cubic-bezier(0, 0.3, 0.6, 1);
-    transition-duration: 150ms;
+  transition-property: box-shadow, border-radius, opacity;
+  transition-timing-function: cubic-bezier(0, 0.3, 0.6, 1);
+  transition-duration: 150ms;
 }
 
 .custom-shadow {
-    box-shadow: 0px 3px 8px 0px rgba(133, 133, 133, 0.25);
+  box-shadow: 0px 3px 8px 0px rgba(133, 133, 133, 0.25);
 }
 </style>
