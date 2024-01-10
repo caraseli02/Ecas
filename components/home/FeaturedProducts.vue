5bf0c0abbbf44a909467eb1cbb90995aa0da8dba
Vlad <caraseli02@gmail.com>
2024-01-10 08:32:05 +0100

    feat: add banner example to product-blocks page

--- components/home/FeaturedProducts.vue
+++ components/home/FeaturedProducts.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="mb-7 lg:mb-[38px] xl:mb-[58px]">
+    <!-- <section class="mb-7 lg:mb-[38px] xl:mb-[58px]">
         <div class="container px-2 md:px-3 xl:pl-6 overflow-hidden">
             <ProductTabs :filters="filters" @new-products="productList = $event"/>
             <div class="md:flex flex-col xl:grid xl:grid-cols-[auto,1fr]">
@@ -38,7 +38,38 @@
                 />
             </div>
         </div>
-    </section>
+    </section> -->
+    <ProductBlocks masonry-view :rows-number="2" class="mb-7 lg:mb-[38px] xl:mb-[58px]" :filters="filters">
+    <template #banner>
+      <div class="md:pt-3 md:pr-0 h-[calc(100%-30px)] mx-2 md:mx-3 xl:mx-0 xl:mr-3 xl:min-w-[330px] xl:min-h-[592px]">
+                    <div
+                        class="flex h-full gap-4 md:gap-[40px] bg-white rounded-xl shadow-xs p-6 md:m-0 xl:flex-col xl:w-[330px]"
+                    >
+                        <img
+                            :src="BlackFridayItem"
+                            alt="Black Friday Product"
+                            class="h-[100px] w-[120px] md:h-auto md:w-[290px] md:max-h-[240px] xl:w-full"
+                        />
+                        <div class="flex-1 flex gap-4 flex-col justify-center xl:justify-start xl:items-start">
+                            <div class="font-semibold text-sm xl:text-xl">
+                                Exclusive Black Friday
+                            </div>
+                            <p
+                                class="text-xs text-slate-500 md:text-[13px] xl:text-base"
+                            >
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sollicitudin accumsan nisi a porttitor.
+                            </p>
+                            <button
+                                class="flex w-fit bg-blue-500 text-[13px] font-medium px-3 py-1.5 rounded-lg text-white md:text-sm md:px-6 md:py-[11px] xl:mx-auto xl:mt-6"
+                            >
+                                View More
+                            </button>
+                        </div>
+
+                    </div>
+                </div>
+    </template>
+    </ProductBlocks>
 </template>
 
 <script setup lang="ts">
