4b820bca89fa1d4e894a36f9f9b0965491535095
Vlad <caraseli02@gmail.com>
2024-01-26 08:00:36 +0100

    feat: add banner component to costumer page

---
+++ components/dashboard/customers/Banner.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="items-stretch shadow-sm bg-white flex w-full max-w-[530px] flex-col pb-3 rounded-xl">
+    <section class="items-stretch bg-gray-700 flex w-full flex-col justify-center px-8 py-2 rounded-t-xl max-md:max-w-full max-md:px-5">
+      <div class="justify-between items-stretch flex gap-5 max-md:max-w-full max-md:flex-wrap">
+        <div class="text-white text-sm font-semibold leading-6 grow shrink basis-auto">October Hot Sale</div>
+        <div class="text-white text-sm font-semibold leading-6 whitespace-nowrap justify-center items-stretch border aspect-[2.04] px-2 rounded-[100px] border-solid border-white">20 %</div>
+      </div>
+    </section>
+    <div class="justify-between items-stretch self-stretch flex max-w-[530px] flex-col pt-3 px-8 max-md:px-5">
+    <header class="max-md:max-w-full">
+      <div class="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
+        <div class="flex flex-col items-stretch w-[22%] max-md:w-full max-md:ml-0">
+        <SvgoNotFoundSmall class="w-[100px] h-[100px]" />
+        </div>
+        <div class="flex flex-col items-stretch w-[78%] ml-5 max-md:w-full max-md:ml-0">
+          <div class="items-stretch flex flex-col text-xs text-zinc-800 font-medium mt-2.5 max-md:mt-7">
+            <div class="text-base leading-6">ADGN2999BCPZ3577XKRT</div>
+            <div class="leading-[167%] mt-1">Voltage Regulators</div>
+            <div class="overflow-hidden text-gray-500 text-ellipsis leading-[133%] mt-1">PHY 1-CH 1.1V/1.8V/2.5V/3.3V 48-Pin</div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="justify-between items-stretch flex w-full gap-5 mt-3 max-md:max-w-full max-md:flex-wrap">
+      <section class="justify-center items-stretch flex flex-col whitespace-nowrap self-start">
+        <div class="text-zinc-800 text-sm leading-4">$ 0,15 (100+)</div>
+        <div class="items-stretch flex justify-between gap-1 text-red-500 mt-1">
+          <div class="text-base font-bold leading-5 grow">$ 0,095</div>
+          <div class="text-sm leading-5 grow">(100+)</div>
+        </div>
+      </section>
+      <div class="justify-between items-stretch bg-sky-500 flex gap-2 px-9 py-2 rounded-lg max-md:px-5">
+        <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/6aad1a87d9e885b79d6137386645dc553a1a21107170084eab1a373cf5b1e34c?apiKey=20497529553648aab918fa2d322ece87&" class="aspect-square object-contain object-center w-6 overflow-hidden shrink-0 max-w-full" />
+        <div class="text-white text-sm font-medium leading-6 grow whitespace-nowrap">Add to cart</div>
+      </div>
+    </div>
+    <div class="bg-neutral-300 self-center flex w-[18px] shrink-0 h-1.5 flex-col mt-5 rounded-3xl"></div>
+  </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
