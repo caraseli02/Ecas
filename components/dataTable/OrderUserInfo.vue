e054767466c80654990a4dcf2d1b8d6b85d2826e
Vlad <caraseli02@gmail.com>
2024-03-04 19:22:02 +0100

    fix: actions buttons styles

--- components/dataTable/OrderUserInfo.vue
+++ components/dataTable/OrderUserInfo.vue
@@ -1,8 +1,10 @@
 <template>
-  <ul class="flex flex-col min-w-[228px]">
-    <li>{{ name }}</li>
-    <li>{{ email }}</li>
-  </ul>
+  <UiButton variant="ghost" class="hover:text-blue-500">
+    <ul class="flex flex-col min-w-[228px]">
+      <li>{{ name }}</li>
+      <li>{{ email }}</li>
+    </ul>
+  </UiButton>
 </template>
 
 <script setup lang="ts">
