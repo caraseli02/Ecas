00e7dc085c3326136faa40922c5614b75cd5ed79
tudorstanciulescu <32811555+tudorstanciulescu@users.noreply.github.com>
2024-04-10 01:42:59 +0300

    wip shipping billing recently bougth and changed account information

--- components/dashboard/client/AddressCards.vue
+++ components/dashboard/client/AddressCards.vue
@@ -1,36 +1,30 @@
 <template>
-  <div v-for="(address, index) in addresses" :key="index" aria-label="Shipping" role="group" class="items-stretch shadow-xs bg-white flex w-full lg:max-w-[476px] xl:max-w-md flex-col p-6 rounded-xl">
+  <div
+      v-for="(address, index) in addresses" :key="index" aria-label="Shipping" role="group"
+      class="items-stretch shadow-xs bg-white flex w-full lg:max-w-[476px] xl:max-w-md flex-col p-6 rounded-xl">
     <section class="justify-between items-stretch flex gap-5">
-      <div class="text-neutral-800 text-sm font-medium leading-5">{{ address.type }}</div>
-      <button><SvgoEditGray24 class="text-slate-300 hover:text-blue-500"/></button>
+      <div class="text-neutral-800 text-sm font-medium leading-5">{{ index === 0 ? 'Shipping' : 'Billing' }}</div>
+      <button>
+        <SvgoEditGray24 class="text-slate-300 hover:text-blue-500"/>
+      </button>
     </section>
     <div aria-label="Address Details" role="group" class="items-stretch flex justify-between gap-3 mt-6">
-      <SvgoAddressBuilding />
+      <SvgoAddressBuilding/>
       <div role="form" class="items-stretch flex grow basis-[0%] flex-col text-sm text-neutral-800">
         <div class="font-medium leading-5">{{ address.alias }}</div>
-        <div class="leading-5 mt-1">{{ address.name }}</div>
-        <div class="leading-5 mt-1">{{ address.fullAddress }}</div>
+        <div class="leading-5 mt-1">{{ address.name1 }}</div>
+        <div class="leading-5 mt-1">{{ address.name2 }}</div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import {ShippingAddressInterface} from '~/types/auth/user-interface';
 
-const addresses = ref([
-  {
-    type: 'Shipping',
-    alias: 'Address Alias 1',
-    name: 'Madalina Popescu',
-    fullAddress: '5073 Mark Brown Rd, NE Dalton, Georgia (GA), 30721, United States'
-  },
-  {
-    type: 'Billing',
-    alias: 'Address Alias 2',
-    name: 'Madalina Popescu',
-    fullAddress: '5073 Mark Brown Rd, NE Dalton, Georgia (GA), 30721, United States'
-  }
-]);
+defineProps<{
+  addresses: Array<ShippingAddressInterface>
+}>()
 
 </script>
 
--- pages/dashboard/client/[id].vue
+++ pages/dashboard/client/[id].vue
@@ -1,113 +1,134 @@
 <template>
-    <section class="flex flex-col justify-center mb-16">
-        <div class="max-w-full p-4 mx-auto transition-all duration-300 !pb-0 md:py-6 lg:px-6 xl:p-0 xl:pt-8">
-            <div class="grid grid-cols-1 gap-4 md:gap-6 w-[358px] md:w-[736px] lg:w-[976px] xl:w-[1392px]">
-                <section class="flex justify-between gap-6 flex-wrap xl:flex-nowrap">
-                    <DashboardClientActiveOrders :items="OrdersIds" />
-                    <DashboardClientAnalytics />
-                    <DashboardClientBanner />
-                </section>
-                <DashboardClientTabBar v-model="activeOrderFilter" />
-                <ClientTableOrder v-if="activeOrderFilter.value === 'orders'" />
-                <ClientTableTransaction v-if="activeOrderFilter.value === 'transaction_history'" />
-                <ClientOnly>
-                    <DashboardClientActivity :data="myActivityData" />
-                </ClientOnly>
-                <!-- <DashboardClientInfoCards :id="route.params.slug" /> -->
-                <div class="flex flex-wrap gap-4 xl:grid-cols-3 xl:grid-rows-[repeat(2,auto)] md:gap-6">
-                    <DashboardClientInfo
-                        :id="myAccountInformation._id"
-                        :account-information="myAccountInformation"
-                        class="xl:col-start-1 xl:row-start-1 xl:row-span-2"
-                    />
-                    <DashboardClientRecentlyBought />
-                    <DashboardClientRecentlyBoughtSlider />
-                    <section class="hidden xl:flex flex-col gap-6 min-w-[330px]">
-                        <DashboardClientCredit
-                            :available-credit="myAccountInformation.adminSettings?.customerCredit?.spent || 0"
-                            :balance="myAccountInformation.adminSettings?.customerCredit?.available || 0"
-                        />
-                        <DashboardClientSupport />
-                    </section>
-                    <div class="flex gap-6 w-full flex-wrap">
-                        <DashboardClientCredit
-                            :available-credit="myAccountInformation.adminSettings?.customerCredit?.available || 0"
-                            :balance="myAccountInformation.adminSettings?.customerCredit?.available || 0"
-                            class="xl:hidden"
-                        />
-                        <DashboardClientAddressCards />
-                        <DashboardClientPaymentCard />
-                        <DashboardClientSupport class="xl:hidden" />
-                    </div>
-                </div>
-            </div>
+  <section class="flex flex-col justify-center mb-16">
+    <div class="max-w-full p-4 mx-auto transition-all duration-300 !pb-0 md:py-6 lg:px-6 xl:p-0 xl:pt-8">
+      <div class="grid grid-cols-1 gap-4 md:gap-6 w-[358px] md:w-[736px] lg:w-[976px] xl:w-[1392px]">
+        <section class="flex justify-between gap-6 flex-wrap xl:flex-nowrap">
+          <DashboardClientActiveOrders :items="OrdersIds"/>
+          <DashboardClientAnalytics/>
+          <DashboardClientBanner/>
+        </section>
+        <DashboardClientTabBar v-model="activeOrderFilter"/>
+        <ClientTableOrder v-if="activeOrderFilter.value === 'orders'"/>
+        <ClientTableTransaction v-if="activeOrderFilter.value === 'transaction_history'"/>
+        <ClientOnly>
+          <DashboardClientActivity :data="myActivityData"/>
+        </ClientOnly>
+        <!-- <DashboardClientInfoCards :id="route.params.slug" /> -->
+        <div class="flex flex-wrap gap-4 xl:grid-cols-3 xl:grid-rows-[repeat(2,auto)] md:gap-6">
+          <DashboardClientInfo
+              :id="myAccountInformation?._id"
+              :account-information="myAccountInformation"
+              class="xl:col-start-1 xl:row-start-1 xl:row-span-2"
+          />
+          <DashboardClientRecentlyBought :data="myRecentlyBougth"/>
+          <DashboardClientRecentlyBoughtSlider/>
+          <section class="hidden xl:flex flex-col gap-6 min-w-[330px]">
+            <DashboardClientCredit
+                :available-credit="myAccountInformation.adminSettings?.customerCredit?.spent || 0"
+                :balance="myAccountInformation.adminSettings?.customerCredit?.available || 0"
+            />
+            <DashboardClientSupport/>
+          </section>
+          <div class="flex gap-6 w-full flex-wrap">
+            <DashboardClientCredit
+                :available-credit="myAccountInformation.adminSettings?.customerCredit?.available || 0"
+                :balance="myAccountInformation.adminSettings?.customerCredit?.available || 0"
+                class="xl:hidden"
+            />
+            <DashboardClientAddressCards :addresses="myAddresses"/>
+            <DashboardClientPaymentCard/>
+            <DashboardClientSupport class="xl:hidden"/>
+          </div>
         </div>
-        <DashboardClientViewHistory />
-    </section>
+      </div>
+    </div>
+    <DashboardClientViewHistory/>
+  </section>
 </template>
 
 <script setup lang="ts">
-import { CustomerDashboardActivityData } from '~/model/dashboard/customer-information/customer-information';
-import { UserDetails } from '~/types/auth/user-details';
+import {CustomerDashboardActivityData} from '~/model/dashboard/customer-information/customer-information';
 
-const { $api } = useNuxtApp();
+import {ShippingAddressInterface, UserInterface} from '~/types/auth/user-interface';
+import {useAuthStore} from '~/store/authStore';
+
+
+const {$api} = useNuxtApp();
+
+const userDetails = useAuthStore().userDetails
 
 // Remove after integration
 const OrdersIds = ref([] as any);
 const activeOrderFilter = ref({
-    icon: 'dashboard',
-    value: 'home',
+  icon: 'dashboard',
+  value: 'home',
 });
 const myActivityData = ref<CustomerDashboardActivityData>({} as CustomerDashboardActivityData);
-const myAccountInformation = ref<UserDetails>({} as UserDetails);
+const myAccountInformation = ref<UserInterface>({} as UserInterface);
+const myRecentlyBougth = ref<any>([] as any)
+const myAddresses = ref<ShippingAddressInterface[]>([] as ShippingAddressInterface[])
+
 
 const activeOrders = async () => {
-    const { data, status } = await $api.customerDashboard.fetchCustomerActiveOrders();
-    if (status && data && data.total) {
-        OrdersIds.value = data.total.map((object) => object.shortId);
-    }
+  const {data, status} = await $api.customerDashboard.fetchCustomerActiveOrders();
+  if (status && data && data.total) {
+    OrdersIds.value = data.total.map((object) => object.shortId);
+  }
 };
 
 const activityWidgets = async () => {
-    const favorites = await $api.customerDashboard.fetchFavorites();
-    const totalOrders = await $api.customerDashboard.fetchTotalOrders();
-    const returns = await $api.customerDashboard.fetchReturns();
+  const favorites = await $api.customerDashboard.fetchFavorites();
+  const totalOrders = await $api.customerDashboard.fetchTotalOrders();
+  const returns = await $api.customerDashboard.fetchReturns();
 
-    if (favorites.status === 'success') {
-        myActivityData.value.favorites = {
-            products: favorites.data.products_number || 0,
-            folders: favorites.data.folders_number || 0,
-        };
-    }
-    if (totalOrders.status === 'success') {
-        myActivityData.value.totalOrders = totalOrders.data.total_orders || 0;
-        myActivityData.value.monthOrder = totalOrders.data.total_orders_current_month || 0;
-    }
+  if (favorites.status === 'success') {
+    myActivityData.value.favorites = {
+      products: favorites.data.products_number || 0,
+      folders: favorites.data.folders_number || 0,
+    };
+  }
+  if (totalOrders.status === 'success') {
+    myActivityData.value.totalOrders = totalOrders.data.total_orders || 0;
+    myActivityData.value.monthOrder = totalOrders.data.total_orders_current_month || 0;
+  }
 
-    if (returns.status === 'success') {
-        myActivityData.value.returns = returns.data.total || 0;
-    }
+  if (returns.status === 'success') {
+    myActivityData.value.returns = returns.data.total || 0;
+  }
 };
 
 const customerInformation = async () => {
-    const accountInformation = await $api.customerDashboard.fetchCustomerAccountInformation();
-    
-    if (accountInformation.status === 'success') {
-        myAccountInformation.value = accountInformation.data;
-    }
-};
+  if (userDetails) {
+    myAccountInformation.value = userDetails
+    console.log(userDetails)
+  }
+}
 
-await Promise.all([activeOrders(), activityWidgets(), customerInformation()]);
+const recentlyBougth = async () => {
+  const {data, status} = await $api.customerDashboard.fetchRecentlyBought()
+  if (status === 'success') {
+    myRecentlyBougth.value = data.total
+  }
+}
+
+const addresses = async () => {
+  if (userDetails && userDetails.personalDetails && userDetails.personalDetails?.shippingAddress) {
+    myAddresses.value.push(userDetails.personalDetails?.shippingAddress[0])
+    myAddresses.value.push(userDetails.personalDetails?.shippingAddress[0])
+  }
+}
+
+await Promise.all([activeOrders(), activityWidgets(), customerInformation(), recentlyBougth(), addresses()]);
 </script>
 
 <style>
 .v-enter-active,
 .v-leave-active {
-    transition: opacity 0.5s ease;
+  transition: opacity 0.5s ease;
 }
 
 .v-enter-from,
 .v-leave-to {
-    opacity: 0;
+  opacity: 0;
 }
 </style>
