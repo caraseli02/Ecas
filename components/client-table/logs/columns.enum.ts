5756750eba4ad24e765ac2a7a975dfb9453f9755
Vlad <caraseli02@gmail.com>
2024-07-08 16:51:43 +0200

    fix: remove EventType from logs

--- components/client-table/logs/columns.enum.ts
+++ components/client-table/logs/columns.enum.ts
@@ -4,12 +4,5 @@ export enum LogsTableColumnsEnum {
     CREATED_AT = 'createdAt',
 }
 
-export enum EventType {
-    user_welcome = 'User Welcome',
-    user_create  = 'User Create',
-    user_login = 'User Login',
-    product_get = 'Product Get'
-}
-
-export interface LogsItem { type: EventType, ip: string, createdAt: string }
+export interface LogsItem { type: string, ip: string, createdAt: string }
 export interface LogsApiResponse {status: string, data: LogsItem[] }
--- components/client-table/logs/columns.ts
+++ components/client-table/logs/columns.ts
@@ -1,19 +1,19 @@
 import type { ColumnDef } from '@tanstack/vue-table';
 import ColumnHeader from '~/components/dataTable/ColumnHeader.vue';
 import CellDate from '~/components/dataTable/CellDate.vue';
-import { EventType, LogsItem, LogsTableColumnsEnum } from '~/components/client-table/logs/columns.enum';
+import { LogsItem, LogsTableColumnsEnum } from '~/components/client-table/logs/columns.enum';
 
 
 export const columns: ColumnDef<LogsItem>[] = [
     {
         accessorKey: LogsTableColumnsEnum.TYPE,
         header: ({ column }) => h(ColumnHeader, { column, title: 'Action' }),
-        cell: ({ row }) => h('div', { class: 'inline overflow-hidden' }, EventType[row.original.type as unknown as keyof typeof EventType]),
+        cell: ({ row }) => h('div', { class: 'inline overflow-hidden capitalize' }, row.original.type.replace('_',' ')  ),
     },
     {
         accessorKey: LogsTableColumnsEnum.IP,
         header: ({ column }) => h(ColumnHeader, { column, title: 'IP' }),
-        cell: ({ row }) => h('div', { class: 'inline overflow-hidden' },  row.original.ip || '-'),
+        cell: ({ row }) => h('div', { class: 'inline overflow-hidden' },  row.original.ip || 'N/A'),
     },
     {
         accessorKey: LogsTableColumnsEnum.CREATED_AT,
