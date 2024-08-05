41c51d4f19c0f542551612bb8de99377ff58ce8a
tudorstanciulescu <32811555+tudorstanciulescu@users.noreply.github.com>
2024-08-06 02:36:21 +0300

    wip alerts and notifications

--- components/dashboard/client/settings/Credit.vue
+++ components/dashboard/client/settings/Credit.vue
@@ -1,60 +1,69 @@
 <script setup lang="ts">
 import { CreditCard } from 'lucide-vue-next';
-const availableCredit = ref(2438.77);
-const balance = ref(7561.23);
+
+import { useAuthStore } from '~/store/authStore';
+import { storeToRefs } from 'pinia';
+
+const authStore = useAuthStore();
+const { getUserDetails } = storeToRefs(authStore);
+
+console.log(getUserDetails);
+
+const availableCredit = ref(getUserDetails.value.adminSettings?.customerCredit?.available || 0);
+const balance = ref(getUserDetails.value.adminSettings?.customerCredit?.spent || 0);
 
 const creditUsagePercentage = computed(() => {
-  const totalCredit = availableCredit.value + balance.value;
-  return (balance.value / totalCredit) * 100;
+    const totalCredit = availableCredit.value + balance.value;
+    return (balance.value / totalCredit) * 100;
 });
 
 const handlePayNow = () => {
-  console.log('Pay now clicked');
+    console.log('Pay now clicked');
 };
 </script>
 
 <template>
-  <section class="flex flex-col self-stretch p-4 md:p-6 bg-white rounded-xl shadow-l">
-    <h2 class="text-xl font-semibold leading-7 text-neutral-800 max-md:max-w-full">
-      Account Credit
-    </h2>
-    <div class="flex flex-col py-6 mt-8 bg-white rounded-xl max-md:max-w-full">
-      <div class="flex gap-2 justify-between leading-[133%] max-md:flex-wrap max-md:max-w-full">
-        <div class="flex flex-col justify-center">
-          <p class="text-2xl font-semibold tracking-normal text-neutral-700">
-            € {{ availableCredit.toFixed(2) }}
-          </p>
-          <p class="mt-1 text-xs font-medium text-slate-500">
-            Available Credit
-          </p>
+    <section class="flex flex-col self-stretch p-4 md:p-6 bg-white rounded-xl shadow-l">
+        <h2 class="text-xl font-semibold leading-7 text-neutral-800 max-md:max-w-full">Account Credit</h2>
+        <div class="flex flex-col py-6 mt-8 bg-white rounded-xl max-md:max-w-full">
+            <div class="flex gap-2 justify-between leading-[133%] max-md:flex-wrap max-md:max-w-full">
+                <div class="flex flex-col justify-center">
+                    <p class="text-2xl font-semibold tracking-normal text-neutral-700">€ {{ availableCredit.toFixed(2) }}</p>
+                    <p class="mt-1 text-xs font-medium text-slate-500">Available Credit</p>
+                </div>
+                <svg
+                    class="text-slate-300 shrink-0 self-start w-6 aspect-square lucide lucide-ellipsis-vertical"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                >
+                    <circle cx="12" cy="12" r="1" />
+                    <circle cx="12" cy="5" r="1" />
+                    <circle cx="12" cy="19" r="1" />
+                </svg>
+            </div>
+            <div class="flex flex-col justify-center mt-6 bg-gray-200 rounded-full max-md:max-w-full">
+                <div
+                    class="shrink-0 h-3 bg-emerald-500 rounded-full max-md:max-w-full"
+                    :style="{ width: `${creditUsagePercentage}%` }"
+                ></div>
+            </div>
+            <div class="flex gap-5 justify-between mt-4 w-full max-md:flex-wrap max-md:max-w-full">
+                <div class="flex gap-2">
+                    <p class="text-base font-medium leading-6 text-slate-500">Balance:</p>
+                    <p class="text-lg font-semibold leading-6 text-neutral-700">€ {{ balance.toFixed(2) }}</p>
+                </div>
+                <button class="flex gap-1 text-base font-semibold leading-6 text-slate-500" @click="handlePayNow">
+                    <CreditCard class="shrink-0 w-6 aspect-square" />
+                    <span>Pay now</span>
+                </button>
+            </div>
         </div>
-        <svg 
-        class="text-slate-300 shrink-0 self-start w-6 aspect-square lucide lucide-ellipsis-vertical"
-        xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="1"/><circle cx="12" cy="5" r="1"/><circle cx="12" cy="19" r="1"/></svg>
-      </div>
-      <div class="flex flex-col justify-center mt-6 bg-gray-200 rounded-full max-md:max-w-full">
-        <div
-          class="shrink-0 h-3 bg-emerald-500 rounded-full max-md:max-w-full"
-          :style="{ width: `${creditUsagePercentage}%` }"
-        ></div>
-      </div>
-      <div class="flex gap-5 justify-between mt-4 w-full max-md:flex-wrap max-md:max-w-full">
-        <div class="flex gap-2">
-          <p class="text-base font-medium leading-6 text-slate-500">Balance:</p>
-          <p class="text-lg font-semibold leading-6 text-neutral-700">
-            € {{ balance.toFixed(2) }}
-          </p>
-        </div>
-        <button
-          class="flex gap-1 text-base font-semibold leading-6 text-slate-500"
-          @click="handlePayNow"
-        >
-          <CreditCard
-            class="shrink-0 w-6 aspect-square"
-          />
-          <span>Pay now</span>
-        </button>
-      </div>
-    </div>
-  </section>
+    </section>
 </template>
