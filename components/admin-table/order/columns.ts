0e5449d9f331ab37ca538a09e42dd4a99629a173
Amzarescu Laurentiu <amzarescu41@gmail.com>
2024-08-08 13:15:57 +0300

    change placeholder for missing email

--- components/admin-table/order/columns.ts
+++ components/admin-table/order/columns.ts
@@ -35,8 +35,8 @@ export const columns: ColumnDef<OrderTableColumns>[] = [
         header: ({ column }) => h(ColumnHeader, { column, title: 'Name' }),
         cell: ({ row }) =>
             h(UserInfo, {
-                name: row.getValue(OrderTableColumnsEnum.USER_NAME) ?? 'add userName',
-                email: row.original.userEmail ?? 'add userEmail',
+                name: row.getValue(OrderTableColumnsEnum.USER_NAME) ?? 'N/A',
+                email: row.original.userEmail ?? 'N/A',
             }),
     },
     {
