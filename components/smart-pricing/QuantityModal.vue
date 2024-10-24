c00e0fccf258ec11fe1d68bb3b4550ae3c1dc2b4
Vlad <caraseli02@gmail.com>
2024-10-24 22:24:53 +0200

    feat: improve smart pricing quantity creation logic

--- components/smart-pricing/QuantityModal.vue
+++ components/smart-pricing/QuantityModal.vue
@@ -6,57 +6,58 @@ const isOpen = ref(false);
 const quantityList = ref([{ id: 1, min: 0, max: 0 }]);
 
 function addQuantity() {
-    quantityList.value.push({ id: quantityList.value.length + 1, min: 0, max: 0 });
+  quantityList.value.push({ id: quantityList.value.length + 1, min: 0, max: 0 });
 }
 
 function removeQuantity(id: number) {
-    quantityList.value = quantityList.value.filter((q) => q.id !== id);
+  quantityList.value = quantityList.value.filter((q) => q.id !== id);
+}
+
+function updateQuantity(id: number, quantity: { min: number; max: number }) {
+  const index = quantityList.value.findIndex((q) => q.id === id);
+  if (index !== -1) {
+    quantityList.value[index] = { ...quantityList.value[index], ...quantity };
+  }
 }
 
 const createNewQuantityTemplate = async () => {
-    console.log(quantityList.value);
+  console.log(quantityList.value);
 };
-
-// function updateQuantity(id: number, quantity: { min: number; max: number }) {
-//   const index = quantityList.value.findIndex(q => q.id === id);
-//   if (index !== -1) {
-//     quantityList.value[index] = { ...quantityList.value[index], ...quantity };
-//   }
-// }
 </script>
 
 <template>
-    <UiDialog v-model:open="isOpen">
-        <UiDialogTrigger as-child>
-            <UiButton class="justify-start hover:text-blue-500" variant="ghost">Quantity</UiButton>
-        </UiDialogTrigger>
-        <UiDialogContent class="max-w-sm p-4 sm:max-w-[480px] gap-10 border-none rounded-xl shadow-s">
-            <UiDialogHeader>
-                <UiDialogTitle>Create Quantity Template</UiDialogTitle>
-            </UiDialogHeader>
-            <section class="flex flex-col gap-4">
-                <SmartPricingQuantityRange
-                    v-for="quantity in quantityList"
-                    :key="quantity.id"
-                    :quantity="quantity"
-                    @delete="removeQuantity(quantity.id)"
-                />
-            </section>
-            <UiDialogFooter class="sm:justify-start">
-                <UiButton type="button" class="min-w-10 h-10 p-0" variant="secondary" @click="addQuantity()">
-                    <PlusIcon class="w-6 h-6" />
-                </UiButton>
-                <UiButton
-                    class="w-full"
-                    type="button"
-                    @click="
-                        createNewQuantityTemplate();
-                        isOpen = false;
-                    "
-                >
-                    Create
-                </UiButton>
-            </UiDialogFooter>
-        </UiDialogContent>
-    </UiDialog>
+  <UiDialog v-model:open="isOpen">
+    <UiDialogTrigger as-child>
+      <UiButton class="justify-start hover:text-blue-500" variant="ghost">Quantity</UiButton>
+    </UiDialogTrigger>
+    <UiDialogContent class="max-w-sm p-4 sm:max-w-[480px] gap-10 border-none rounded-xl shadow-s">
+      <UiDialogHeader>
+        <UiDialogTitle>Create Quantity Template</UiDialogTitle>
+      </UiDialogHeader>
+      <section class="flex flex-col gap-4">
+        <SmartPricingQuantityRange
+          v-for="quantity in quantityList"
+          :key="quantity.id"
+          :quantity="quantity"
+          @delete="removeQuantity(quantity.id)"
+          @updateRange="updateQuantity(quantity.id, $event)"
+        />
+      </section>
+      <UiDialogFooter class="sm:justify-start">
+        <UiButton type="button" class="min-w-10 h-10 p-0" variant="secondary" @click="addQuantity()">
+          <PlusIcon class="w-6 h-6" />
+        </UiButton>
+        <UiButton
+          class="w-full"
+          type="button"
+          @click="
+            createNewQuantityTemplate();
+            isOpen = false;
+          "
+        >
+          Create
+        </UiButton>
+      </UiDialogFooter>
+    </UiDialogContent>
+  </UiDialog>
 </template>
