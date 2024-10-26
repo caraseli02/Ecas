5247a273b3a1a164d72fcefe8bd649c2ffde2a94
Vlad <caraseli02@gmail.com>
2024-10-26 18:37:21 +0200

    feat: Side menu items update and integration. points 4 to 9

--- components/layout/account-modal/Links.vue
+++ components/layout/account-modal/Links.vue
@@ -9,9 +9,15 @@
       </div>
     </NuxtLink>
     <NuxtLink v-for="(menuItem, index) in menuItems" :key="index" :to="menuItem.to"
-      class=" hover:bg-zinc-100 text-neutral-800 hover:text-blue-500 items-stretch flex justify-start gap-3 pl-3 pr-20 py-3 rounded-lg">
+      class="items-stretch flex justify-start gap-3 pl-3 pr-20 py-3 rounded-lg"
+      :class="menuItem.disabled ? 'text-neutral-200 cursor-not-allowed' : 'hover:bg-zinc-100 text-neutral-800 hover:text-blue-500'"
+      @click="emit('close')"
+      >
       <component :is="menuItem.icon" class="w-6 h-6" />
-      <div class="transition-all text-sm font-medium leading-6 whitespace-nowrap">
+      <div v-if="menuItem.disabled" class="transition-all text-sm font-medium leading-6 whitespace-nowrap">
+        {{ menuItem.text }}
+      </div>
+      <div v-else class="transition-all text-sm font-medium leading-6 whitespace-nowrap">
         {{ menuItem.text }}
       </div>
       <div v-if="menuItem.text === 'Notifications'"
@@ -23,15 +29,15 @@
 </template>
 
 <script setup lang="ts">
-import IconApps from '@/assets/icons/menu/apps.svg?component';
-import SvgoMenuNotification from '@/assets/icons/menu/Notification.svg?component';
-import SvgoMenuCartAdd from '@/assets/icons/menu/cart-add.svg?component';
-import SvgoMenuTag from '@/assets/icons/menu/tag.svg?component';
-import SvgoMenuFileUpload from '@/assets/icons/menu/file-upload.svg?component';
-import SvgoMenuHeart from '@/assets/icons/menu/heart.svg?component';
-import SvgoMenuPackagingLocation from '@/assets/icons/menu/packaging-location.svg?component';
-import SvgoMenuClock from '@/assets/icons/menu/Clock.svg?component';
-import SvgoMenuSetting from '@/assets/icons/menu/Setting.svg?component';
+import IconApps from '@/assets/icons/menu/apps.svg';
+import SvgoMenuNotification from '@/assets/icons/menu/Notification.svg';
+import SvgoMenuCartAdd from '@/assets/icons/menu/cart-add.svg';
+import SvgoMenuTag from '@/assets/icons/menu/tag.svg';
+import SvgoMenuFileUpload from '@/assets/icons/menu/file-upload.svg';
+import SvgoMenuHeart from '@/assets/icons/menu/heart.svg';
+import SvgoMenuPackagingLocation from '@/assets/icons/menu/packaging-location.svg';
+import SvgoMenuClock from '@/assets/icons/menu/Clock.svg';
+import SvgoMenuSetting from '@/assets/icons/menu/Setting.svg';
 import { useAuthStore } from '~/store/authStore';
 import { AccountRole } from '~/types/signup/form';
 
@@ -42,15 +48,13 @@ const emit = defineEmits<{
 }>();
 
 const route = useRoute()
-console.log(route);
-
 
 const isAdmin = computed(() => {
   const role = AccountRole[authStore.userDetails?.role as number]
   return role === 'SuperAdmin' || role === 'Admin'
 })
 
-const menuItems = [
+const menuItems = ref([
   // {
   //   icon: IconApps,
   //   text: 'Dashboard',
@@ -70,11 +74,13 @@ const menuItems = [
     icon: SvgoMenuTag,
     text: 'Price Offer',
     to: '/',
+    disabled: true,
   },
   {
     icon: SvgoMenuFileUpload,
     text: 'BOM Upload',
     to: '/',
+    disabled: true,
   },
   {
     icon: SvgoMenuHeart,
@@ -84,18 +90,18 @@ const menuItems = [
   {
     icon: SvgoMenuPackagingLocation,
     text: 'Order Tracking',
-    to: '/',
+    to: isAdmin.value ? '/dashboard' : '/dashboard/client?tab=home'
   },
   {
     icon: SvgoMenuClock,
     text: 'Order History',
-    to: '/',
+    to:  isAdmin.value ? '/dashboard/orders' : '/dashboard/client?tab=orders',
   },
   {
     icon: SvgoMenuSetting,
     text: 'Settings',
-    to: '/',
+    to: isAdmin.value ? '/dashboard' : '/dashboard/client?tab=settings'
   },
   // Add more menu items as needed
-]
+])
 </script>
