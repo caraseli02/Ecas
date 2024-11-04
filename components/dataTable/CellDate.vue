5816098d5e2c8e31f50061ba89736f10e0e0b4ad
Amzarescu Laurentiu <amzarescu41@gmail.com>
2024-11-04 18:51:18 +0200

    fix date error

--- components/dataTable/CellDate.vue
+++ components/dataTable/CellDate.vue
@@ -11,18 +11,6 @@ const props = defineProps<{
 }>();
 
 const formattedDate = computed(() => {
-    // const date = new Date(props.inputDateString);
-    const date = moment(props.inputDateString).format('DD MMMM YYYY HH:mm');
-
-    const options: Intl.DateTimeFormatOptions = {
-        day: '2-digit', // Numeric day (with leading zero if needed)
-        month: 'long', // Full month name
-        year: 'numeric', // Full year
-        hour: '2-digit', // 24-hour format
-        minute: '2-digit',
-        timeZone: 'UTC', // Keep UTC if this is important
-    };
-
-    return date; // Use 'en-GB' locale
+    return moment(props.inputDateString).format('DD MMMM YYYY HH:mm');
 });
 </script>
