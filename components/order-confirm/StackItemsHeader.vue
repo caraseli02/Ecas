ea62115b19ec04370bf33b691bf3bbc2134f1de8
Vlad <caraseli02@gmail.com>
2024-11-06 07:37:35 +0100

    feat: improve statuses UI for order page

--- components/order-confirm/StackItemsHeader.vue
+++ components/order-confirm/StackItemsHeader.vue
@@ -1,47 +1,68 @@
 <script setup lang="ts">
 import { DotsVerticalIcon } from '@radix-icons/vue';
-import { MapPin } from 'lucide-vue-next';
+import { MapPin, CheckCircle } from 'lucide-vue-next';
+
+const isAdmin = ref(true);
 </script>
 
 <template>
   <section class="flex flex-col">
     <div class="flex justify-end flex-wrap gap-3 items-center w-full max-md:max-w-full">
       <div class="flex gap-3 items-center self-stretch my-auto text-sm leading-none">
-        <h2 class="self-stretch my-auto text-gray-500">Order Status</h2>
-        <div class="flex overflow-hidden gap-2 justify-center items-center self-stretch my-auto whitespace-nowrap rounded-md text-zinc-800">
-          <span class="flex shrink-0 self-stretch my-auto w-3 h-3 bg-emerald-500 rounded-full fill-emerald-500" aria-hidden="true"></span>
-          <span>Completed</span>
-        </div>
+        <h4 class="self-stretch my-auto text-gray-500">Order Status</h4>
+       <OrderConfirmStatusSelect v-if="isAdmin"></OrderConfirmStatusSelect>
+       <OrderConfirmStatusDisplay status-color="bg-emerald-500" status-text="Completed" v-else />
       </div>
       <div class="flex gap-3 items-center self-stretch my-auto">
-        <UiButton size="xs" class="flex overflow-hidden justify-center items-center self-stretch px-4 py-0 my-auto text-sm font-medium leading-6 text-white bg-sky-500 rounded-md">
+        <UiBadge v-if="isAdmin"
+          class="font-medium text-white bg-emerald-500 hover:bg-emerald-500">
+          <CheckCircle class="object-contain shrink-0 self-stretch my-auto w-4 aspect-square" />
+          <span class="self-stretch py-1 pl-2 my-auto">Order Completed</span>
+        </UiBadge>
+        <UiButton v-if="!isAdmin" size="xs"
+          class="flex overflow-hidden justify-center items-center self-stretch px-4 py-0 my-auto text-sm font-medium leading-6 text-white bg-sky-500 rounded-md">
           <MapPin class="object-contain shrink-0 self-stretch my-auto w-4 aspect-square" />
           <span class="self-stretch py-1 pl-2 my-auto">Track Order</span>
         </UiButton>
-        <button class="flex overflow-hidden justify-center items-center self-stretch p-1 my-auto w-8 h-8 rounded-md bg-zinc-100" aria-label="Additional options">
-          <DotsVerticalIcon alt="" class="object-contain self-stretch my-auto w-6 aspect-square" />
-        </button>
+        <UiPopover>
+          <UiPopoverTrigger>
+            <UiButton
+              variant="ghost"
+              size="icon"
+              class="flex overflow-hidden justify-center items-center self-stretch p-1 my-auto w-8 h-8 rounded-md bg-zinc-100"
+              aria-label="Additional options">
+              <DotsVerticalIcon alt="" class="object-contain self-stretch my-auto w-6 aspect-square" />
+            </UiButton>
+          </UiPopoverTrigger>
+          <UiPopoverContent class="w-fit flex flex-col gap-2 p-2" align="end">
+            <UiButton class="hover:text-blue-500 justify-start min-w-[164px]" variant="ghost" size="sm">
+              <CheckCircle class="mr-1 object-contain shrink-0 self-stretch my-auto w-4 aspect-square" />
+              Mark as Paid
+            </UiButton>
+            <UiButton class="hover:text-blue-500 justify-start min-w-[164px]" variant="ghost" size="sm">
+              <MapPin class="mr-1 object-contain shrink-0 self-stretch my-auto w-4 aspect-square" />
+              Track Order
+            </UiButton>
+          </UiPopoverContent>
+        </UiPopover>
       </div>
     </div>
-    <div class="flex flex-wrap gap-10 justify-between items-start p-4 mt-4 w-full rounded-lg border border-solid bg-stone-50 border-grey-300 max-md:max-w-full">
+    <div
+      class="flex flex-wrap gap-10 justify-between items-start p-4 mt-4 w-full rounded-lg border border-solid bg-stone-50 border-grey-300 max-md:max-w-full">
       <div class="flex gap-4 items-center min-h-[20px]">
         <span class="self-stretch my-auto text-sm leading-none text-gray-500">Order Type</span>
         <div class="flex overflow-hidden gap-2 justify-center items-center self-stretch my-auto rounded-md">
-          <span class="overflow-hidden self-stretch p-0.5 my-auto w-4 h-4 text-xs font-medium leading-none text-center text-white whitespace-nowrap bg-emerald-500 rounded-[100px]">S</span>
+          <span
+            class="overflow-hidden self-stretch p-0.5 my-auto w-4 h-4 text-xs font-medium leading-none text-center text-white whitespace-nowrap bg-emerald-500 rounded-[100px]">S</span>
           <span class="self-stretch my-auto text-sm leading-none text-zinc-800">Stock Order</span>
         </div>
       </div>
       <div class="flex gap-3 items-center text-sm leading-none min-h-[20px]">
         <span class="self-stretch my-auto text-gray-500">Payment Status</span>
-        <div class="flex overflow-hidden gap-2 justify-center items-center self-stretch my-auto whitespace-nowrap rounded-md text-zinc-800">
-          <span class="flex shrink-0 self-stretch my-auto w-3 h-3 bg-emerald-500 rounded-full fill-emerald-500" aria-hidden="true"></span>
-          <span>Card</span>
-        </div>
+        <OrderConfirmStatusDisplay status-color="bg-emerald-500" status-text="Card" />
       </div>
     </div>
   </section>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ components/order-confirm/StatusSelect.vue
@@ -0,0 +1,54 @@
+<template>
+  <UiSelect :default-value="statuses[0].value">
+    <UiSelectTrigger class="w-[150px] h-8">
+      <UiSelectValue placeholder="Order Status" />
+    </UiSelectTrigger>
+    <UiSelectContent>
+      <UiSelectGroup>
+        <UiSelectItem class="p-2" v-for="status in statuses" :key="status.value" :value="status.value">
+          <div class="flex items-center gap-2">
+          <p class="w-3 h-3 rounded-full" :class="status.color" />
+          {{ status.label }}
+          </div>
+        </UiSelectItem>
+      </UiSelectGroup>
+    </UiSelectContent>
+  </UiSelect>
+</template>
+
+<script setup lang="ts">
+const statuses = ref([
+{
+  value: 'completed',
+  label: 'Completed',
+  color: 'bg-green-500'
+},  
+{
+  value: 'pending',
+  label: 'Pending',
+  color: 'bg-orange-500'
+},
+{
+  value: 'processing',
+  label: 'Processing',
+  color: 'bg-purple-500'
+},
+{
+  value: 'dispatched',
+  label: 'Dispatched',
+  color: 'bg-blue-500'
+},
+{
+  value: 'delivered',
+  label: 'Delivered',
+  color: 'bg-blue-500'
+},
+{
+  value: 'cancelled',
+  label: 'Cancelled',
+  color: 'bg-red-500'
+}
+])
+</script>
+
+<style scoped></style>
