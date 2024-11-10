981d7d124a2af5a52d2a236923dc36b5cfa5e855
Amzarescu Laurentiu <amzarescu41@gmail.com>
2024-11-11 00:01:08 +0200

    add stripe secret

--- plugins/firebase.client.ts
+++ plugins/firebase.client.ts
@@ -1,13 +1,10 @@
-import dotenv from 'dotenv';
 import { initializeApp } from '@firebase/app';
 import { getAuth } from '@firebase/auth';
-
-dotenv.config({ path: '.env' });
 
 export default defineNuxtPlugin(async (nuxtApp) => {
     const config = useRuntimeConfig();
     console.log('config: ', config);
-    console.log('Initializing Firebase client', import.meta.env);
+    console.log('Initializing Firebase client', process.env);
     // const app = initializeApp({
     //     apiKey: process.env.VITE_FIREBASE_API_KEY as string,
     //     authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN as string,
@@ -18,13 +15,13 @@ export default defineNuxtPlugin(async (nuxtApp) => {
     //     measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID as string,
     // });
     const app = initializeApp({
-        apiKey: process.env.NUXT_FIREBASE_API_KEY as string,
-        authDomain: process.env.NUXT_FIREBASE_AUTH_DOMAIN as string,
-        projectId: process.env.NUXT_FIREBASE_PROJECT_ID as string,
-        storageBucket: process.env.NUXT_FIREBASE_STORAGE_BUCKET as string,
-        messagingSenderId: process.env.NUXT_FIREBASE_MESSAGE_SENDER_ID as string,
-        appId: process.env.NUXT_FIREBASE_APP_ID as string,
-        measurementId: process.env.NUXT_FIREBASE_MEASUREMENT_ID as string,
+        apiKey: (config.FIREBASE_API_KEY as string) || 'test',
+        authDomain: (config.FIREBASE_AUTH_DOMAIN as string) || 'test',
+        projectId: (config.FIREBASE_PROJECT_ID as string) || 'test',
+        storageBucket: (config.FIREBASE_STORAGE_BUCKET as string) || 'test',
+        messagingSenderId: (config.FIREBASE_MESSAGE_SENDER_ID as string) || 'test',
+        appId: (config.FIREBASE_APP_ID as string) || 'test',
+        measurementId: (config.FIREBASE_MEASUREMENT_ID as string) || 'test',
     });
 
     const auth = getAuth(app);
