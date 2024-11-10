3b8ce58a65964dd6338dcc153c72a3211be8ffaa
Amzarescu Laurentiu <amzarescu41@gmail.com>
2024-11-10 21:09:17 +0200

    add stripe secret

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -93,7 +93,7 @@ export default defineNuxtConfig({
     },
     runtimeConfig: {
         FIREBASE_API_KEY: process.env.NUXT_FIREBASE_API_KEY,
-        FIREBASE_AUTH_DOMAIN: process.env.NUXT_FIREBASE_AUTH_DOMAIN,
+        FIREBASE_AUTH_DOMAIN: process.env.VITE_FIREBASE_AUTH_DOMAIN,
         FIREBASE_PROJECT_ID: process.env.NUXT_FIREBASE_PROJECT_ID,
         FIREBASE_STORAGE_BUCKET: process.env.NUXT_FIREBASE_STORAGE_BUCKET,
         FIREBASE_MESSAGE_SENDER_ID: process.env.NUXT_FIREBASE_MESSAGE_SENDER_ID,
