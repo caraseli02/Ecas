69797023a8952ee2be20bc0712f31c86b52b1072
Amzarescu Laurentiu <amzarescu41@gmail.com>
2024-11-10 22:26:41 +0200

    add stripe secret

--- .docker/nuxt/docker_entrypoint.sh
+++ .docker/nuxt/docker_entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 set -e
+# Capture the output of `printenv`
+output=$(printenv)
+# Use a regular expression to filter the desired variables
+filtered_output=$(echo "$output" | grep -E '^VITE_|^NUXT_')
 
-# Capture environment variables starting with VITE_ and NUXT_
-env | grep ^VITE_ | grep -vE '^(_|PATH|PWD|HOME|BASH|SHLVL|USER|...)' >> .env
-env | grep ^NUXT_ | grep -vE '^(_|PATH|PWD|HOME|BASH|SHLVL|USER|...)' >> .env
+# Print the filtered output
+echo "$filtered_output"
 
 echo "Environment variables written to .env"
 
