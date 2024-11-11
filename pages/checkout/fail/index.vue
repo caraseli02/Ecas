271bc75809dd4d7067b4aea6121d7262d0b08ba1
Vlad <caraseli02@gmail.com>
2024-11-11 17:55:45 +0100

    feat: remove error message from checkout fail page when click retry

--- pages/checkout/fail/index.vue
+++ pages/checkout/fail/index.vue
@@ -1,22 +1,24 @@
 <template>
-    <div class="container flex items-center flex-col gap-8 my-10 min-h-[50vh]" v-if="true">
-        <section class="w-full bg-light-100 border-[1.5px] border-red-500 rounded-lg p-6 flex flex-col justify-center items-center gap-4">
-            <h4 class="text-2xl font-medium">Payment failed</h4>
-            <AlertTriangleIcon class="text-red-500 w-10 h-10 stroke-2" />
-            <p>Reason: {{ getPreviousCheckoutError?.message || 'Something went wrong' }}</p>
-        </section>
-        <div class="flex gap-4">
-            <UiButton class="w-fit" variant="secondary" @click="
-                cancelOrder();
-            router.push({ path: `/dashboard/client` });
-            ">
-                <span class="text-sm leading-[1.42857] font-medium"> Cancel order </span>
-            </UiButton>
-            <UiButton @click="retry = true" class="w-fit">
-                <span class="text-sm leading-[1.42857] font-medium"> Try Again </span>
-            </UiButton>
-        </div>
+    <div class="container flex items-center flex-col gap-8 my-10 min-h-[50vh]" v-if="getOrderClientSecret">
         <PaymentSessionCardForm v-if="retry" />
+        <template v-else>
+            <section class="w-full bg-light-100 border-[1.5px] border-red-500 rounded-lg p-6 flex flex-col justify-center items-center gap-4">
+                <h4 class="text-2xl font-medium">Payment failed</h4>
+                <AlertTriangleIcon class="text-red-500 w-10 h-10 stroke-2" />
+                <p>Reason: {{ getPreviousCheckoutError?.message || 'Something went wrong' }}</p>
+            </section>
+            <div class="flex gap-4">
+                <UiButton class="w-fit" variant="secondary" @click="
+                    cancelOrder();
+                router.push({ path: `/dashboard/client` });
+                ">
+                    <span class="text-sm leading-[1.42857] font-medium"> Cancel order </span>
+                </UiButton>
+                <UiButton @click="retry = true" class="w-fit">
+                    <span class="text-sm leading-[1.42857] font-medium"> Try Again </span>
+                </UiButton>
+            </div>
+        </template>
     </div>
     <div v-else>No data available</div>
 </template>
