cb50fe712a9e896b38fd6b506c124dbfd50f7685
Vlad <caraseli02@gmail.com>
2024-11-12 22:43:30 +0100

    feat: app in testing modal and small Ui changes

--- components/order-confirm/StatusDisplay.vue
+++ components/order-confirm/StatusDisplay.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex overflow-hidden gap-2 justify-center items-center self-stretch my-auto whitespace-nowrap rounded-md text-zinc-800">
+    class="flex overflow-hidden gap-2 justify-center items-center self-stretch whitespace-nowrap rounded-md text-zinc-800">
     <span :class="statusColor" class="flex shrink-0 self-stretch my-auto w-3 h-3 rounded-full" aria-hidden="true"></span>
     <span class="capitalize">{{ statusText }}</span>
   </div>
