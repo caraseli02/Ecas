619741327b2ede89f6c933e52d55c56fd3f69bcf
Vlad <caraseli02@gmail.com>
2024-11-15 16:57:35 +0100

    fix: top search bar colapse logic

--- components/layout/header/Search.vue
+++ components/layout/header/Search.vue
@@ -22,12 +22,12 @@
                     "
                 />
             </form>
-            <div @click="handleEnterButton" v-if="!isScrolled" class="flex items-center justify-center bg-blue-500 cursor-pointer px-4 py-3">
+            <div @click="handleEnterButton" v-if="!isScrolled" class="flex items-center justify-center bg-blue-500 hover:bg-blue-600 cursor-pointer px-4 py-3">
                 <SearchIcon class="w-5 h-5 text-white" />
             </div>
-            <div @click="handleEnterButton" v-else class="absolute top-1/2 right-3 -translate-y-1/2">
+            <UiButton size="icon" variant="ghost" @click="handleEnterButton" v-else class="absolute top-1/2 right-3 -translate-y-1/2 cursor-pointer">
                 <SearchIcon class="w-5 h-5 text-gray-500" />
-            </div>
+            </UiButton>
         </label>
         <Transition name="fade">
             <LayoutHeaderSearchResults
