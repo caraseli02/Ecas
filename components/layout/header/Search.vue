1717baf851314c3c1bb0745d913d442a1b87f637
Vlad <caraseli02@gmail.com>
2024-11-15 16:18:05 +0100

    fix: search button and search modal

--- components/layout/header/Search.vue
+++ components/layout/header/Search.vue
@@ -22,10 +22,10 @@
                     "
                 />
             </form>
-            <div v-if="!isScrolled" class="flex items-center justify-center bg-blue-500 cursor-pointer px-4 py-3">
+            <div @click="handleEnterButton" v-if="!isScrolled" class="flex items-center justify-center bg-blue-500 cursor-pointer px-4 py-3">
                 <SearchIcon class="w-5 h-5 text-white" />
             </div>
-            <div v-else class="absolute top-1/2 right-3 -translate-y-1/2">
+            <div @click="handleEnterButton" v-else class="absolute top-1/2 right-3 -translate-y-1/2">
                 <SearchIcon class="w-5 h-5 text-gray-500" />
             </div>
         </label>
--- components/layout/header/SearchResults.vue
+++ components/layout/header/SearchResults.vue
@@ -1,6 +1,7 @@
 <template>
     <div
-        class="absolute z-50 -bottom-2.5 translate-y-full left-0 w-full bg-white border border-[#F2F2F2] rounded-lg py-2 pl-2 pr-0.5 shadow-m md:rounded-xl md:-bottom-0.5"
+        v-if="isLoading || props.products"
+        class="absolute z-50 -bottom-2.5 translate-y-full left-0 w-full bg-white border border-light-300 rounded-lg py-2 pl-2 pr-0.5 shadow-md md:rounded-xl md:-bottom-0.5"
         :style="{
             boxShadow: '0px 4px 12px rgba(51, 51, 51, 0.1)',
         }"
@@ -8,35 +9,38 @@
         <div v-if="isLoading">
             <p class="p-2 rounded text-slate-500 transition-colors duration-300">Loading...</p>
         </div>
-        <div v-else class="grid grid-cols-1 max-h-[340px] overflow-auto scrollbar-thin pr-1.5">
-            <NuxtLink
-                v-for="(product, index) in props?.products"
-                v-if="props?.products?.length > 0"
-                :key="index"
-                :to="`/product/${product._id}`"
-                class="p-2 rounded-lg text-slate-500 transition-colors duration-300 hover:bg-[#F2F2F2]"
-            >
-                <div
-                    class="flex items-center justify-between gap-1 text-xs leading-[1.67] font-medium text-neutral-700 mb-1 lg:text-sm lg:leading-[1.43]"
-                >
-                    <div class="flex items-center w-[calc(100%-50px)] hover:text-blue-500">
-                        <div class="truncate">{{ productTitle(product) }}</div>
-                        <CaretIcon class="w-4 h-4 flex-shrink-0" />
-                        <div class="font-Inter">{{ product.name }}</div>
-                    </div>
-                    <div
-                        class="rounded-3xl bg-blue-500 px-1 text-xs leading-[1.33] font-semibold text-white flex-shrink-0 lg:translate-y-2.5"
-                        :class="[product.adminSettings?.featured === true ? 'invisible' : '']"
+        <div v-else>
+            <div v-if="props.products.length > 0" class="grid grid-cols-1 max-h-[340px] overflow-auto scrollbar-thin pr-1.5">
+                <template v-for="(product, index) in props.products" :key="product._id">
+                    <NuxtLink
+                        :to="`/product/${product._id}`"
+                        class="p-2 rounded-lg text-slate-500 transition-colors duration-300 hover:bg-light-300"
                     >
-                        New
-                    </div>
-                </div>
-                <div class="text-xs leading-[1.33] truncate text-slate-500 lg:w-[calc(100%-50px)] hover:text-blue-500">
-                    {{ product.description }}
-                </div>
-            </NuxtLink>
+                        <div
+                            class="flex items-center justify-between gap-1 text-xs leading-[1.67] font-medium text-neutral-700 mb-1 lg:text-sm lg:leading-[1.43]"
+                        >
+                            <div class="flex items-center w-[calc(100%-50px)] hover:text-blue-500">
+                                <div class="truncate">{{ productTitle(product) }}</div>
+                                <CaretIcon class="w-4 h-4 flex-shrink-0" />
+                                <div class="font-Inter">{{ product.name }}</div>
+                            </div>
+                            <div
+                                v-if="!product.adminSettings?.featured"
+                                class="rounded-3xl bg-blue-500 px-1 text-xs leading-[1.33] font-semibold text-white flex-shrink-0 lg:translate-y-2.5"
+                            >
+                                New
+                            </div>
+                        </div>
+                        <div class="text-xs leading-[1.33] truncate text-slate-500 lg:w-[calc(100%-50px)] hover:text-blue-500">
+                            {{ product.description }}
+                        </div>
+                    </NuxtLink>
+                </template>
+            </div>
             <div v-else>
-                <p class="p-2 rounded text-slate-500 transition-colors duration-300 hover:bg-gray-100 hover:text-blue-500">No data found</p>
+                <p class="p-2 rounded text-slate-500 transition-colors duration-300 hover:bg-gray-100 hover:text-blue-500">
+                    No data found
+                </p>
             </div>
         </div>
     </div>
@@ -62,6 +66,6 @@ const productTitle = (product: ProductInterface) => {
     const mapLabels = mapLabelsToIds(categories.value);
     const categoriesLabelsArray = mapPathArrayOfNames(product.path, mapLabels);
 
-    return categoriesLabelsArray[categoriesLabelsArray.length - 1].name;
+    return categoriesLabelsArray[categoriesLabelsArray.length - 1]?.name || 'N/A';
 };
 </script>
