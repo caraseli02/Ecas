0627ef442c18c24fd983a630e17c3b59960d48db
Amzarescu Laurentiu <amzarescu41@gmail.com>
2024-11-20 22:31:52 +0200

    fix login with google

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,17 +1,17 @@
-import { storeToRefs } from 'pinia';
 import { useAuthStore } from '~~/store/authStore';
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
     const token = useCookie('token');
     const authStore = useAuthStore();
     // redirect the user to the home page
+    console.log('token', token.value);
     if (!token?.value) {
         return navigateTo({
             path: '/',
         });
     }
 
-    if(import.meta.client && !token?.value) {
+    if (import.meta.client && !token?.value) {
         authStore.signOut();
-    };
+    }
 });
