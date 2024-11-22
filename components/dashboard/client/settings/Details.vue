35d37dce76f24c7b6b8d4ee8787de49d46adc3cc
Vlad <caraseli02@gmail.com>
2024-11-22 17:24:00 +0100

    fix: user edit validation

--- components/dashboard/client/settings/Details.vue
+++ components/dashboard/client/settings/Details.vue
@@ -184,14 +184,14 @@ const formBusinessSchema = toTypedSchema(
             .string({
                 required_error: 'Tax ID is required',
             })
-            .optional()
-            .default(getUserDetails.value.companyDetails?.taxId || ''),
+            .default(getUserDetails.value.companyDetails?.taxId || '')
+            .optional(),
         vatNumber: z
             .string({
                 required_error: 'V.A.T Number is required',
             })
-            .optional()
-            .default(getUserDetails.value.companyDetails?.vat || ''),
+            .default(getUserDetails.value.companyDetails?.vat || '')
+            .optional(),
         bankName: z
             .string()
             .optional()
@@ -199,9 +199,9 @@ const formBusinessSchema = toTypedSchema(
             .or(z.literal('')),
         iban: z
             .union([z.string().length(0), z.string().regex(/^[A-Z]{2}\d{2}[A-Z0-9]{1,30}$/, 'Invalid IBAN')])
-            .optional()
             .default(getUserDetails.value.companyDetails?.bank_iban || '')
-            .transform((e: string) => (e === '' ? undefined : e)),
+            .transform((e: string) => (e === '' ? undefined : e))
+            .optional(),
     })
 );
 
