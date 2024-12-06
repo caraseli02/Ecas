eaeec737f625b01e29356e92cbce2b73a9f6deb0
Vlad <caraseli02@gmail.com>
2024-12-06 19:01:02 +0100

    feat: wip main menu

---
+++ components/layout/header/main-menu/BannerNew.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel'
+</script>
+
+<template>
+  <Carousel class="relative w-full max-h-[572px] bg-white rounded-xl border border-gray-200 border-solid shadow-sm max-md:px-5 max-md:py-24 focus-visible:ring-0">
+    <CarouselContent>
+      <CarouselItem
+        v-for="(_, index) in 5"
+        :key="index"
+      >
+        <div class="p-1">
+          <section
+            class="flex overflow-hidden relative justify-between items-start self-stretch px-6 py-64 h-full"
+            role="region"
+            aria-labelledby="hero-title"
+          >
+            <div
+              class="flex absolute -right-7 z-0 shrink-0 h-[272px] min-w-[240px] top-[67px] w-[554px] bg-gradient-to-r"
+              aria-hidden="true"
+            />
+            <div
+              class="flex absolute bottom-32 z-0 flex-col font-medium leading-6 left-[100px] min-w-[240px] max-w-[415px] text-zinc-800"
+            >
+              <h1
+                id="hero-title"
+                class="text-5xl font-bold"
+              >
+                Scale Your Business Beyond Limits
+              </h1>
+              <p class="mt-6 text-base">
+                Join our growing community of successful vendors. Sign up for our
+                waiting list today!
+              </p>
+              <button
+                class="flex overflow-hidden justify-center items-center self-start px-4 mt-6 text-sm text-white bg-sky-500 rounded-lg transition-colors duration-300 hover:bg-sky-600 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2"
+              >
+                <span class="self-stretch p-2 my-auto">Book a Demo</span>
+              </button>
+            </div>
+          </section>
+        </div>
+      </CarouselItem>
+    </CarouselContent>
+    <CarouselPrevious class="left-10" />
+    <CarouselNext class="right-10" />
+  </Carousel>
+</template>
--- components/layout/header/main-menu/Popover.vue
+++ components/layout/header/main-menu/Popover.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
-import { MenuIcon, ChevronRight, ArrowRight, GemIcon, HeadsetIcon, Building2Icon } from 'lucide-vue-next'
+import { MenuIcon, ChevronRight, ArrowRight, GemIcon, HeadsetIcon, Building2Icon, ChevronLeftIcon } from 'lucide-vue-next'
 import { ref, computed } from 'vue'
+import { breakpointsTailwind, useBreakpoints } from '@vueuse/core'
 import Breadcrumb from './Breadcrumb.vue'
 import CardPlaceholderSmall from '@/assets/icons/card-placeholder-small.svg'
 
 defineProps<{
   isScrolled: boolean
 }>()
+
+const breakpoints = useBreakpoints(breakpointsTailwind)
+const smallerThanSm = breakpoints.smaller('sm') // sm and larger
+const lgAndLarger = breakpoints.greaterOrEqual('lg') // sm and larger
 
 const { categories } = useCategories()
 
@@ -44,11 +49,14 @@ const resetCategories = () => {
   selectedCategories.value = []
 }
 
-const isOpen = ref(false)
+const isOpen = ref(true)
 </script>
 
 <template>
-  <UiPopover v-model:open="isOpen">
+  <UiPopover
+    v-if="lgAndLarger"
+    v-model:open="isOpen"
+  >
     <UiPopoverTrigger as-child>
       <UiButton
         :variant="isOpen ? 'secondary' : 'ghost'"
@@ -67,7 +75,7 @@ const isOpen = ref(false)
     <UiPopoverContent
       align="start"
       :side-offset="25"
-      class="p-0 w-screen bg-transparent shadow-none border-none"
+      class="p-0 w-screen bg-transparent shadow-none border-none hidden lg:block"
     >
       <div class="container ">
         <section
@@ -103,7 +111,10 @@ const isOpen = ref(false)
             </UiButton>
           </div>
           <!-- Right Column: Subcategories -->
-          <div class="flex flex-col gap-8 bg-white px-6 py-4 rounded-xl">
+          <div
+            v-if="currentCategory"
+            class="flex flex-col gap-8 bg-white px-6 py-4 rounded-xl"
+          >
             <!-- Breadcrumb Navigation -->
             <section class="flex justify-between items-center h-8">
               <Breadcrumb :links="selectedCategories.map((link) => link.name)" />
@@ -119,15 +130,14 @@ const isOpen = ref(false)
             </section>
             <!-- Subcategories Display -->
             <section
-              v-if="currentCategory"
               class="h-full"
             >
               <template v-if="currentCategory.subcategory && currentCategory.subcategory.length > 0">
-                <div class="grid grid-cols-3 2xl:grid-cols-4 gap-4 h-full max-h-[384px] overflow-y-auto">
+                <div class="grid grid-cols-[320px_minmax(320px,_1fr)] xl:grid-cols-3 2xl:grid-cols-4 gap-4 h-full max-h-[384px] overflow-y-auto">
                   <div
                     v-for="sub in currentCategory.subcategory"
                     :key="sub.id"
-                    class="flex flex-col gap-4"
+                    class="flex flex-col gap-4 max-w-xs"
                   >
                     <h3
                       class="font-medium"
@@ -207,8 +217,128 @@ const isOpen = ref(false)
               </div>
             </section>
           </div>
+          <LayoutHeaderMainMenuBannerNew v-else />
         </section>
       </div>
     </UiPopoverContent>
   </UiPopover>
+  <UiSheet v-else>
+    <UiSheetTrigger as-child>
+      <UiButton
+        :variant="isOpen ? 'secondary' : 'ghost'"
+        class="flex items-center mr-4 md:mr-0"
+        :class="[{ 'text-white hover:text-blue-500': !isOpen }]"
+      >
+        <MenuIcon class="w-6 h-6" />
+        <span
+          v-if="!isScrolled"
+          class="hidden leading-normal font-medium ml-2 md:inline-block lg:ml-4"
+        >
+          Products
+        </span>
+      </UiButton>
+    </UiSheetTrigger>
+    <UiSheetContent
+      class="px-1 pt-1.5"
+      side="left"
+    >
+      <UiSheetHeader>
+        <UiSheetTitle v-if="currentCategory">
+          <UiButton
+            variant="link"
+            class="flex items-center mr-4 md:mr-0"
+            @click="resetCategories"
+          >
+            <ChevronLeftIcon class="w-4 h-4" />
+            <span
+              v-if="!isScrolled"
+              class="hidden leading-normal font-medium ml-2 md:inline-block lg:ml-4"
+            >
+              Back
+            </span>
+          </UiButton>
+          <UiSeparator class="my-1" />
+        </UiSheetTitle>
+      </UiSheetHeader>
+      <div
+        v-if="currentCategory"
+        class="flex flex-col ml-4 overflow-auto max-h-screen pb-32"
+      >
+        <div
+          v-for="sub in currentCategory.subcategory"
+          :key="sub.id"
+          class="flex flex-col gap-4 max-w-xs"
+        >
+          <h3
+            class="font-medium"
+          >
+            {{ sub.name }}
+          </h3>
+          <div
+            v-if="sub.subcategory && sub.subcategory.length > 0"
+            class="flex flex-col gap-3 items-start"
+          >
+            <div
+              v-for="childSub in sub.subcategory.slice(0, 8)"
+              :key="childSub.id"
+            >
+              <UiButton
+                variant="link"
+                class="text-left pl-0 flex-col items-start"
+                :disabled="childSub.productCount === 0"
+                @click="onCategoryClick(childSub, false)"
+              >
+                {{ childSub.name }}
+                <span class="text-xs">{{ childSub.productCount }} items</span>
+              </UiButton>
+              <UiBadge
+                v-if="false"
+                class="bg-blue-500 text-xs px-2"
+              >
+                New
+              </UiBadge>
+              <UiBadge
+                v-if="false"
+                class="bg-rose-600 text-xs px-2"
+              >
+                Sale
+              </UiBadge>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Left Column: Main Categories -->
+      <div
+        v-else
+        class="w-full flex flex-col items-start"
+      >
+        <UiButton
+          v-for="category in categories"
+          :key="category.id"
+          class="text-start h-11 w-full max-w-[366px] flex flex-row flex-nowrap justify-between gap-2"
+          variant="ghost"
+          @click="onCategoryClick(category, true)"
+        >
+          <div class="flex gap-2 items-center">
+            <component
+              :is="category.icon"
+              v-if="category.icon && category.icon !== 'N/A'"
+              class="min-w-5 min-h-5 text-black"
+            />
+            <CardPlaceholderSmall
+              v-else
+              class="min-w-6 min-h-6 text-black"
+            />
+            <p class="flex flex-col items-start w-full font-medium">
+              {{ category.name }}
+              <span class="text-xs font-normal text-slate-500">
+                {{ category.productCount }}
+              </span>
+            </p>
+          </div>
+          <ChevronRight class="w-4 h-4 text-black" />
+        </UiButton>
+      </div>
+    </UiSheetContent>
+  </UiSheet>
 </template>
