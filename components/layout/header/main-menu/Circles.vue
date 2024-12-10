6be56c2555c223e16de4b1e27cb8010d398f29b0
Vlad <caraseli02@gmail.com>
2024-12-10 23:13:40 +0100

    feat: improve UI

--- components/layout/header/main-menu/Circles.vue
+++ components/layout/header/main-menu/Circles.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="relative w-full max-w-lg blur-xl">
-    <div class="absolute top-0 left-0 w-72 h-72 bg-[#325DF8] opacity-50 rounded-full mix-blend-multiply filter blur-xl animate-blob" />
-    <div class="absolute top-0 right-0 w-72 h-72 bg-[#5C28DF] opacity-50 rounded-full mix-blend-multiply filter blur-xl animate-blob animation-delay-2000" />
+    <div class="absolute top-0 left-0 w-96 h-96 bg-[#325DF8] opacity-50 rounded-full mix-blend-multiply filter blur-xl animate-blob" />
+    <div class="absolute top-36 -right-10 w-96 h-96 bg-[#5C28DF] opacity-50 rounded-full mix-blend-multiply filter blur-xl animate-blob animation-delay-2000" />
     <!-- <div class="absolute -bottom-8 left-20 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000" /> -->
   </div>
 </template>
